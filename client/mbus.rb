933ede31f8fb1f9e4103fc2c744cd4659cbb4a5d
Noah Gibbs <noah@ooyala.com>
2012-01-09 14:00:32 -0800

    More Stomp UI sketching

--- client/mbus.rb
+++ client/mbus.rb
@@ -13,6 +13,11 @@ module MBus
     @stomp_client = OnStomp.connect(*args)
   end
 
+  def set_uuid(uuid)
+    @uuid = uuid
+    update_subscriptions
+  end
+
   module Topic
     def send(topic, json_hash)
       @stomp_client.send(topic, MultiJson.encode(json_hash))
@@ -23,9 +28,9 @@ module MBus
     # general delivery.
     def receive_async(topic, &block)
       if block_given?
-        @stomp_client.subscribe(topic, :ack => :none, &block)
+        @stomp_client.subscribe(topic, &block)
       else
-        raise "Need block for now!"
+        @stomp_client.subscribe(topic, @general_delivery_block)
       end
     end
   end
@@ -38,25 +43,31 @@ module MBus
     # This subscribes asynchronously to only this
     # queue.  Without a block, subscribe for
     # general delivery.
-    def receive_async(queue_name)
+    def receive_async(queue_name, &block)
       if block_given?
         @stomp_client.subscribe(topic, :ack => :client, &block)
       else
-        raise "Need block for now!"
+        @stomp_client.subscribe(topic, &@general_delivery_block)
       end
     end
   end
 
   module Direct
-    def send_uuid(uuid)
-      raise "No direct messages implemented yet!"
+    def send_uuid(uuid, json_hash)
+      @stomp_client.send(uuid, MultiJson.encode(json_hash))
     end
   end
 
-  # This subscribes to direct messages, and subscriptions for
+  # This subscribes to direct messages and subscriptions for
   # general delivery.
-  def subscribe_async
-    raise "General delivery not implemented yet!"
+  def subscribe_async(&block)
+    @general_delivery_block = block
+
+    update_subscriptions
+  end
+
+  def update_subscriptions
+    @stomp_client.subscribe(@uuid, &@general_delivery_block) if @uuid && @general_delivery_block
   end
 
 end
