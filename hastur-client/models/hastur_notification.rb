b8eacc1f16af8cbdad9ff66f7e0866eea5824ba1
Viet Nguyen <viet@ooyala.com>
2012-01-19 17:51:10 -0800

    [hastur-client] Change the constructor of HasturNotification to make it easy for testing.

--- hastur-client/models/hastur_notification.rb
+++ hastur-client/models/hastur_notification.rb
@@ -7,12 +7,12 @@ require "uuid"
 module Hastur
   class Notification
     attr_accessor :name, :subsystem, :uuid, :id
-    def initialize(name, subsystem, uuid, time = nil)
+    def initialize(name, subsystem, uuid, time = nil, id = nil)
       @name = name
       @subsystem = subsystem
       @uuid = uuid
-      @id = UUID.new.generate
-      @time = time.nil? ? Time.new : time   # timestamp this notification
+      @id = id.nil? ? UUID.new.generate : id    # allow IDs to be passed in (motivation was for testing)
+      @time = time.nil? ? Time.new : time       # timestamp this notification
     end
 
     def to_json
