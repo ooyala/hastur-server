e99411fa05c3d41d4fe794e45304ee175e4f4b9d
Noah Gibbs <noah@ooyala.com>
2012-01-22 15:20:19 -0800

    First draft of JSON and multi-server stuff for basic router

--- read-and-print/basic-router.rb
+++ read-and-print/basic-router.rb
@@ -2,26 +2,43 @@
 
 require 'rubygems'
 require 'ffi-rzmq'
+require 'yajl-ruby'
+require 'multi_json'
+require 'socket'
 require 'trollop'
+
+MultiJson.engine = :yajl
+
+METHODS = [ :register, :notify, :stats, :heartbeat ]
 
 opts = Trollop::options do
   banner <<-EOS
 basic-router.rb - a simple 0mq router.  Clients connect to the router URI,
-  servers connect to the push URI.
+  servers connect to the server URIs.
 
   Options:
 EOS
   opt :router_uri, "ZMQ Router URI", :default => "tcp://127.0.0.1:4321", :type => String
-  opt :push_uri, "ZMQ Push URI", :default => "tcp://127.0.0.1:4322", :type => String
-  opt :pub_uri,  "ZMQ Pub URI", :default => "tcp://127.0.0.1:4323", :type => String
+  opt :pub_uri,  "ZMQ Pub URI", :default => "tcp://127.0.0.1:4322", :type => String
+
+  port = 4330
+  METHODS.each do |method|
+    opt "#{method}_uri".to_sym, "ZMQ #{method} server URI",
+      :default => "tcp://127.0.0.1:#{port}", :type => String
+    port += 1
+  end
+
+  opt :error_uri, "ZMQ Error server URI", :default => "tcp://127.0.0.1:4350", :type => String
   opt :linger,  "set ZMQ_LINGER",   :default => 1,                 :type => Integer
   opt :hwm,     "set ZMQ_HWM",      :default => 1,                 :type => Integer
   opt :timeout, "poll timeout",     :default => 0.1
 end
 
-[:router_uri, :push_uri, :pub_uri].each do |opt|
+method_uris = METHODS.map(:to_s).map { |s| s + "_uri" }.map(:to_sym)
+
+(method_uris + [:router_uri, :pub_uri, :error_uri]).each do |opt|
   if opts[opt] !~ /\w+:\/\/[^:]+:\d+/
-    raise "Option --#{opt} must be of the form protocol://hostname:port rather than #{opts[opt]}"
+    Trollop::die opt, "Option --#{opt} must be of the form protocol://hostname:port rather than #{opts[opt]}"
   end
 
   if ZMQ::LibZMQ.version2? && opts[opt] =~ /\Wlocalhost\W/
@@ -36,19 +53,48 @@ STDERR.puts "Using ZeroMQ version #{version}"
 
 ctx = ZMQ::Context.new(1)
 
+def socket_for_type_and_uri(socket_type, uri)
+  socket = ctx.socket(const_get("ZMQ::#{socket_type.to_s.upcase}"))
+
+  # These aren't strictly necessary, but the behavior they enable is
+  # what we usually expect.  For now, have all sockets use the same
+  # options.  Set socket options *before* bind or connect.
+  socket.setsockopt(ZMQ::LINGER, opts[:linger]) # flush messages before shutdown
+  socket.setsockopt(ZMQ::HWM, opts[:hwm]) # high water mark, the number of buffered messages
+
+  socket.bind uri
+
+  STDERR.puts "New #{socket_type} socket listening on '#{uri}'."
+end
+
+def multi_recv(socket)
+  messages = []
+  router_socket.recv_string(data = "")
+  messages << data
+  while socket.more_parts?
+    router_socket.recv_string(data = "")
+    messages << data
+  end
+  messages
+end
+
+def multi_send(socket, messages)
+  last_message = messages.pop
+
+  messages.each do |message|
+    socket.send_string(message, ZMQ::SNDMORE)
+  end
+  socket.send_string(last_message)
+end
+
 sockets = {}
-[:router, :push, :pub].each do |socket_type|
-  sockets[socket_type] = ctx.socket(const_get("ZMQ::#{socket_type.upcase}"))
-  address = opts["#{socket_type}_uri".to_sym]
-  sockets[socket_type].bind address
+router_socket = socket_for_type_and_uri(:router, opts[:router_uri])
+pub_socket = socket_for_type_and_uri(:pub, opts[:pub_uri])
+error_socket = socket_for_type_and_uri(:push, opts[:error_uri])
 
-  STDERR.puts "#{socket_type} socket listening on '#{address}'."
-
-  # these aren't strictly necessary, but the behavior they enable is what we usually expect
-  # for now, have router and push sockets both use the same options
-
-  sockets[socket_type].setsockopt(ZMQ::LINGER, opts[:linger]) # flush messages before shutdown
-  sockets[socket_type].setsockopt(ZMQ::HWM, opts[:hwm]) # high water mark, the number of buffered messages
+METHODS.each do |method|
+  uri = opts["#{method}_uri".to_sym]
+  sockets[method] = socket_for_type_and_uri(:push, uri)
 end
 
 # For now, use a simple blocking receive and a simple blocking send.
@@ -56,10 +102,26 @@ end
 # its push-socket receivers hit their high-water marks.  The pub socket
 # should never block regardless.
 loop do
-  data = ""
-  socket[:router].recv_string(data)
-  STDERR.puts "Routing data: #{data}"
+  messages = multi_recv(router_socket)
+  STDERR.puts "Routing data: #{messages.inspect}"
 
-  socket[:pub].send_string data
-  socket[:push].send_string data
+  hash = MultiJson.decode(messages[-1]) rescue nil
+  method = hash['method'] rescue nil
+
+  hash[:router_host] = Socket.gethostname
+  data = MultiJson.encode(hash) rescue nil
+
+  if data
+    messages[-1] = data # New JSON encoding for resend
+    multi_send(pub_socket, messages)
+
+    if sockets[hash['method']]
+      multi_send(sockets[hash['method'].to_sym], messages)
+    else
+      multi_send(sockets[:error], messages)
+    end
+  else
+    # Parse error, forward old data straight to error server
+    multi_send(sockets[:error], messages)
+  end
 end
