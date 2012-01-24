e0fa157d370cb5df3a307e43e1811ef4b8c9870a
Al Tobey <al@ooyala.com>
2012-01-23 23:51:08 -0800

    Convert to use /etc/uuid, ephemeral if absent.
    This should be 100% predictable and usually all set up before a
    client runs. Because we have to test and so on, an ephemeral uuid
    will be generated for each process. We may want to add an option
    to force ephemeral for testing at some point. It's also possible
    we could have boxes that run more than one client service if it's
    acting as a centralized poller, but I suspect that will be a
    separate piece of code.

--- hastur-client/lib/hastur_uuid_utils.rb
+++ hastur-client/lib/hastur_uuid_utils.rb
@@ -4,6 +4,8 @@ require "uuid"
 module Hastur
   module Client
     class UuidUtils
+      UUID_FILE = "/etc/uuid"
+
       include Singleton
 
       attr_accessor :uuid
@@ -13,33 +15,36 @@ module Hastur
       end
 
       #
-      # Saves the Hastur client UUID in the current location under .hastur_client_uuid
-      #
-      def save_uuid( uuid, filepath )
-        unless File.exists?( filepath )
-          # create the file with the uuid
-          File.open(filepath, 'w') {|f| f.write( uuid ) }
-        end
-      end
-
-      #
       # Retrieves the UUID from the current location under .hastur_client_uuid
       # if the file exists. Otherwise return a newly generated UUID.
       #
       def get_uuid
         return @@uuid unless @@uuid.nil?
-        # TODO(viet): figure out how to better deal with the UUID
-        filepath = "#{File.dirname(__FILE__)}/../.hastur_client_uuid"
-        uuid = nil
-        if File.exists?( filepath )
-          # read from file to get the UUID
-          f = File.new( filepath, "r")
-          @@uuid = f.gets.chomp
+
+        # if the system's UUID is set in /etc/uuid (just a single 36-byte ascii UUID in a file, no syntax)
+        # we'll use that, otherwise just assume and complain that this run of the client
+        # is ephemeral and generate a new UUID on-the-fly
+        if File.file? UUID_FILE
+          uuid_in = File.read(UUID_FILE).chomp
+          if uuid_in =~ /^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/i
+            @@uuid = uuid_in
+          else
+            raise "Invalid UUID in #{UUID_FILE}."
+          end
         else
-          # generate a new UUID and save it
+          # generate a new UUID on the fly
           @@uuid = UUID.new.generate
-          save_uuid( uuid, filepath )
+
+          # save it if we can, either running as root or 
+          if (File.exists?(UUID_FILE) and File.writeable?(UUID_FILE)) or File.writable?(File.basename(UUID_FILE))
+            File.open(UUID_FILE, "w") do |file|
+              file.puts @@uuid
+            end
+          else
+            STDERR.puts "Could not persist generated system UUID. This UUID, '#{@@uuid}', is ephemeral."
+          end
         end
+
         @@uuid
       end
     end
