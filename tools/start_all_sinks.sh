74082eb2ba0a5c295d32a72cd2a88c5d2e7659bf
Noah Gibbs <noah@ooyala.com>
2012-01-24 09:20:10 -0800

    Start all sinks *at once*, not serially

--- tools/start_all_sinks.sh
+++ tools/start_all_sinks.sh
@@ -3,10 +3,10 @@
 # Start up one each of every type of sink (a.k.a. worker or server) that the router routes packets to,
 # on the default port the router looks for.
 
-./zmqcli.rb --type pull --connect --prefix [register] --uri tcp://127.0.0.1:4330
-./zmqcli.rb --type pull --connect --prefix [notify] --uri tcp://127.0.0.1:4331
-./zmqcli.rb --type pull --connect --prefix [stats] --uri tcp://127.0.0.1:4332
-./zmqcli.rb --type pull --connect --prefix [heartbeat] --uri tcp://127.0.0.1:4333
-./zmqcli.rb --type pull --connect --prefix [logs] --uri tcp://127.0.0.1:4334
+./zmqcli.rb --type pull --connect --prefix [register] --uri tcp://127.0.0.1:4330 &
+./zmqcli.rb --type pull --connect --prefix [notify] --uri tcp://127.0.0.1:4331 &
+./zmqcli.rb --type pull --connect --prefix [stats] --uri tcp://127.0.0.1:4332 &
+./zmqcli.rb --type pull --connect --prefix [heartbeat] --uri tcp://127.0.0.1:4333 &
+./zmqcli.rb --type pull --connect --prefix [logs] --uri tcp://127.0.0.1:4334 &
 
-./zmqcli.rb --type pull --connect --prefix [errors] --uri tcp://127.0.0.1:4350
+./zmqcli.rb --type pull --connect --prefix [errors] --uri tcp://127.0.0.1:4350 &
