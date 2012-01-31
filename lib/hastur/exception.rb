7f94a0d09452ed6a4fe42ce03f75914ac0fdbdb9
Al Tobey <al@ooyala.com>
2012-01-31 01:33:54 -0800

    Start an exceptions module.

---
+++ lib/hastur/exception.rb
@@ -0,0 +1,11 @@
+# Hastur-specific exceptions
+
+module Hastur
+  # thrown when one of the various packet handlers encounters problems decoding network packets
+  class PacketDecodingError < StandardError
+  end
+
+  class UnsupportedError < StandardError
+  end
+end
+
