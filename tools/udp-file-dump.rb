fa196edd1229c20ff060c81f65586524cbc3ffb4
Al Tobey <al@ooyala.com>
2012-01-30 22:06:40 -0800

    [tools] Clean up udp-file-dump.rb

--- tools/udp-file-dump.rb
+++ tools/udp-file-dump.rb
@@ -1,24 +1,38 @@
 #!/usr/bin/env ruby
 #
-# a simple program that reads UDP port 8125 and records each
-# packet to a file unaltered
-#
-# mostly useful for writing out actual data for testing parsers, etc.
-# like Hastur::Input::Collectd
+# A simple program for recording packets on a UDP port to files.
+# One of the primary use cases is to record packets to use as test input.
+# Could also be handy to debug Hastur UDP clients.
 
 require 'socket'
+require 'trollop'
+
+opts = Trollop::options do
+  banner <<-EOS
+#{$0} - dump UDP packets to files, one packet per file
+
+Example:
+  #{$0} --port 8125 --path /tmp/foo
+
+EOS
+  opt :bind, "address to bind to",          :type => String,  :default  => "127.0.0.1"
+  opt :port, "UDP port to listen on",       :type => Integer, :default  => 8125
+  opt :path, "directory to write files to", :type => String,  :required => true
+end
+
 counter=0
 
 BasicSocket.do_not_reverse_lookup = true
 client = UDPSocket.new
-client.bind('127.0.0.1', 8125)
+client.bind(opts[:bind], opts[:port])
 
 loop do
-  data, addr = client.recvfrom(1500)
+  data, addr = client.recvfrom(16384)
 
-  File.open("/tmp/collectd-packets/#{counter}", "w") do |f|
+  File.open("#{opts[:path]}/#{counter}", "w") do |f|
     f.write data
   end
+
   counter += 1
 end
 
