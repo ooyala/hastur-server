bdd5770edf6b0132c12f57b3a6f541401e759c52
Viet Nguyen <viet@ooyala.com>
2012-01-31 18:08:27 -0800

    [test-harness] Add another assertion library change.

--- experiments/test/heartbeat.rb
+++ experiments/test/heartbeat.rb
@@ -1,11 +1,15 @@
-require_relative "lib/topology_helper"
+$LOAD_PATH.unshift "lib"
+
+require "topology_helper"
+require "assertions"
+
+include Hastur::Test::Assert
 
 def test(topology_file)
   load topology_file
 
   # TODO(viet): Refactor all of this stuff out into its own reusable module
-  t = Hastur::Test::Topology.new
-  t.build(TOPOLOGY)   # TOPOLOGY comes from topology_file
+  t = Hastur::Test::Topology.new(TOPOLOGY)
   t.start_all
 
   # start the actual testcase
@@ -13,8 +17,13 @@ def test(topology_file)
   # TODO(viet): Build a mechanism to retrieve messages from 
   #             each node in the topology and verify that heartbeat
   #             messages are being received.
+  heartbeat_msgs = t.packets(WORKERS[0])
 
   # TODO(viet): assert that all of the heartbeats are showing up
+  assert(2, heartbeat_msgs.size)
+  packet_list_equal([ {:method => "heartbeat", :uuid => CLIENTS[0]},
+                    {:method => "heartbeat", :uuid => CLIENTS[1]} ], heartbeat_msgs)
+
   t.stop CLIENTS[0]
 
   sleep 60
