570ac67b47cd74a88a495700385fc7e64f08c42f
Noah Gibbs <noah@ooyala.com>
2012-02-01 13:46:39 -0800

    Refactor fuzzy_filter

--- lib/hastur/monkeypatch.rb
+++ lib/hastur/monkeypatch.rb
@@ -1,11 +1,7 @@
 class Array
   def fuzzy_filter(h)
     keep_if do |item|
-      is_good = false
-      h.keys.each do |k|
-        is_good = true if h[k] === item[k]
-      end
-      is_good
+      h.keys.all? { |k| h[k] === item[k] }
     end
   end
 end
