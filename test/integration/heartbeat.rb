effcb4d3604f67b24667d03ab6b2105728c53b87
Viet Nguyen <viet@ooyala.com>
2012-02-02 20:49:45 -0800

    Add heartbeat test using the integration test harness.

---
+++ test/integration/heartbeat.rb
@@ -0,0 +1,94 @@
+#!/usr/bin/env ruby
+
+require "test/unit"
+require_relative "./integration_test_helper"
+
+class HeartbeatTest < Test::Unit::TestCase
+
+  def test_heartbeat
+    sleep 6
+    # get messages from the sink shims
+    messages = Hastur::Test::ZMQ.all_packets_to("heartbeat_worker")
+    # verify that the messages on the heartbeat shims are heartbeat messages
+    assert_equal(messages.size, messages.fuzzy_filter( {:method => "heartbeat"} ).size)
+    # verify that the count of messages on the heartbeat shims are accurate
+    assert_equal(1, messages.size)
+  end
+
+  def setup
+    Dir.chdir HASTUR_ROOT
+    processes = [
+                 {
+                   :name => :client1,
+                   :command => "./bin/hastur-client.rb --router <%= zmq[:router] %> --port 8125",
+                   # TODO(noah): mock UDP port to catch or forward messages?
+                 },
+                 {
+                   :name => :client2,
+                   :command => "./bin/hastur-client.rb --router <%= zmq[:router] %> --port 8126",
+                   # TODO(noah): mock UDP port to catch or forward messages?
+                 },
+                 {
+                   :name => :router,
+                   :command => <<EOS ,
+    ./infrastructure/hastur-router.rb --heartbeat-uri <%= zmq[:heartbeat] %>
+                     --register-uri <%= zmq[:register] %>
+                     --notify-uri <%= zmq[:notify] %> --stat-uri <%= zmq[:stat] %>
+                     --log-uri <%= zmq[:log] %> --error-uri <%= zmq[:error] %>
+                     --router-uri <%= zmq[:router] %> --from-sink-uri <%= zmq[:from_sink] %>
+EOS
+                   :resources => {
+                     :zmq => [
+                       { :name => :router, :type => :router, :listen => 4321 },
+                       { :name => :register, :type => :push, :listen => 4330 },
+                       { :name => :notify, :type => :push, :listen => 4331 },
+                       { :name => :stats, :type => :push, :listen => 4332 },
+                       { :name => :heartbeat, :type => :push, :listen => 4333 },
+                       { :name => :logs, :type => :push, :listen => 4334 },
+                       { :name => :errors, :type => :push, :listen => 4350 },
+                       { :name => :pub, :type => :pub, :listen => 4322 },
+                       { :name => :from_sink, :type => :pull, :listen => 4323 }
+                     ],
+                   }
+                 },
+                 {
+                   :name => :notify_worker,
+                   :command => <<EOS ,
+    ./tools/zmqcli.rb --type pull --connect --prefix [notify] --uri <%= zmq[:notify] %>
+EOS
+                 },
+                 {
+                   :name => :stats_worker,
+                   :command => <<EOS ,
+    ./tools/zmqcli.rb --type pull --connect --prefix [stats] --uri <%= zmq[:stats] %>
+EOS
+                 },
+                 {
+                   :name => :heartbeat_worker,
+                   :command => <<EOS ,
+    ./tools/zmqcli.rb --type pull --connect --prefix [heartbeat] --uri <%= zmq[:heartbeat] %>
+EOS
+                 },
+                 {
+                   :name => :logs_worker,
+                   :command => <<EOS ,
+    ./tools/zmqcli.rb --type pull --connect --prefix [logs] --uri <%= zmq[:logs] %>
+EOS
+                 },
+                 {
+                   :name => :errors_worker,
+                   :command => <<EOS ,
+    ./tools/zmqcli.rb --type pull --connect --prefix [errors] --uri <%= zmq[:errors] %>
+EOS
+                 },
+    ]
+
+    @topology = Hastur::Test::Topology.new(processes)
+    @topology.start_all
+  end
+
+  def teardown
+    @topology.stop_all
+  end
+end
+
--- test/integration/integration_test_helper.rb
+++ test/integration/integration_test_helper.rb
@@ -5,6 +5,7 @@ require "bundler"
 # For testing Hastur components, use the local version *first*.
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), "..", "..", "lib")
 
+require "hastur/monkeypatch"
 require "hastur/test/topology_helper"
 
 HASTUR_ROOT = File.join(File.dirname(__FILE__), "..", "..")
