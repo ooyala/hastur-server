c27428b6691e132111efa15ae56291f2786caa18
Viet Nguyen <viet@ooyala.com>
2012-02-06 13:44:55 -0800

    [tests] Add a notification integration test. Does not yet verify with acks yet.

---
+++ test/integration/notification_test.rb
@@ -0,0 +1,113 @@
+#!/usr/bin/env ruby
+
+require "test/unit"
+require_relative "./integration_test_helper"
+
+class NotificationTest < Test::Unit::TestCase
+
+  def test_notification
+    # send a notification
+    sleep 1
+    puts "Sending notification..."
+    u = UDPSocket.new
+    u.send('{ "params":{ "name":"name", "subsystem":"backlot", "uuid":"thisismyuuid", "id":"notification-id"}, "method":"notify" }', 
+           0, "127.0.0.1", 8125)
+    sleep 1
+    # wait for 1 second for notification
+    sleep 1
+    # get messages from the sink shims for notifications
+    sec_1_messages = Hastur::Test::ZMQ.all_payloads_to(:notify)
+    # wait for another 5 seconds for a re-notification
+    sleep 6
+    # get messages from the sink shims for notifications
+    sec_6_messages = Hastur::Test::ZMQ.all_payloads_to(:notify)
+    # verify that the messages on the notify shims are notify messages
+    assert_equal(sec_1_messages.size, sec_1_messages.fuzzy_filter( {"method" => "notify"} ).size)
+    assert_equal(sec_6_messages.size, sec_6_messages.fuzzy_filter( {"method" => "notify"} ).size)
+    # verify that the count of messages on the notify shims are accurate
+    assert_equal(1, sec_1_messages.size)
+    assert_equal(2, sec_6_messages.size)
+  end
+
+  def setup
+    Dir.chdir HASTUR_ROOT
+    processes = [
+                 {
+                   :name => :client1,
+                   :command => "./bin/hastur-client.rb --router <%= zmq[:router] %> --port 8125",
+                   # TODO(noah): mock UDP port to catch or forward messages?
+                 },
+                 {
+                   :name => :client2,
+                   :command => "./bin/hastur-client.rb --router <%= zmq[:router] %> --port 8126",
+                   # TODO(noah): mock UDP port to catch or forward messages?
+                 },
+                 {
+                   :name => :router,
+                   :command => <<EOS ,
+    ./infrastructure/hastur-router.rb --heartbeat-uri <%= zmq[:heartbeat] %>
+                     --register-uri <%= zmq[:register] %>
+                     --notify-uri <%= zmq[:notify] %> --stat-uri <%= zmq[:stat] %>
+                     --log-uri <%= zmq[:log] %> --error-uri <%= zmq[:error] %>
+                     --router-uri <%= zmq[:router] %> --from-sink-uri <%= zmq[:from_sink] %>
+EOS
+                   :resources => {
+                     :zmq => [
+                       { :name => :router, :type => :router, :listen => 4321 },
+                       { :name => :register, :type => :push, :listen => 4330 },
+                       { :name => :notify, :type => :push, :listen => 4331 },
+                       { :name => :stats, :type => :push, :listen => 4332 },
+                       { :name => :heartbeat, :type => :push, :listen => 4333 },
+                       { :name => :logs, :type => :push, :listen => 4334 },
+                       { :name => :errors, :type => :push, :listen => 4350 },
+#                       { :name => :pub, :type => :pub, :listen => 4322 },
+                       { :name => :from_sink, :type => :pull, :listen => 4323 }
+                     ],
+                   }
+                 },
+                 {
+                   :name => :notify_worker,
+                   :command => <<EOS ,
+    ./tools/zmqcli.rb --type pull --connect --prefix [notify] --uri <%= zmq[:notify] %>
+EOS
+                 },
+                 {
+                   :name => :stats_worker,
+                   :command => <<EOS ,
+    ./tools/zmqcli.rb --type pull --connect --prefix [stats] --uri <%= zmq[:stats] %>
+EOS
+                 },
+                 {
+                   :name => :heartbeat_worker,
+                   :command => <<EOS ,
+    ./tools/zmqcli.rb --type pull --connect --prefix [heartbeat] --uri <%= zmq[:heartbeat] %>
+EOS
+                 },
+                 {
+                   :name => :logs_worker,
+                   :command => <<EOS ,
+    ./tools/zmqcli.rb --type pull --connect --prefix [logs] --uri <%= zmq[:logs] %>
+EOS
+                 },
+                 {
+                   :name => :errors_worker,
+                   :command => <<EOS ,
+    ./tools/zmqcli.rb --type pull --connect --prefix [errors] --uri <%= zmq[:errors] %>
+EOS
+                 },
+    ]
+
+    @topology = Hastur::Test::Topology.new(processes)
+    puts "Starting up all of the topology components..."
+    @topology.start_all
+    puts "Started up all of the topology components..."
+  end
+
+  def teardown
+    puts "Tearing down all of the topology components..."
+    @topology.stop_all
+    `pkill -f hastur`
+    puts "Topology is torn down..."
+  end
+end
+
