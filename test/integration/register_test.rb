61e6655b780240fcbf0d6a4cd45dbc6d64725da9
Viet Nguyen <viet@ooyala.com>
2012-02-07 11:16:11 -0800

    [test] Forgot to remove conflicts.

--- test/integration/register_test.rb
+++ test/integration/register_test.rb
@@ -36,13 +36,8 @@ class RegisterTest < Test::Unit::TestCase
                    :command => <<EOS ,
     ./infrastructure/hastur-router.rb --heartbeat-uri <%= zmq[:heartbeat] %>
                      --register-uri <%= zmq[:register] %>
-<<<<<<< Updated upstream
-                     --notify-uri <%= zmq[:notify] %> --stats-uri <%= zmq[:stat] %>
-                     --log-uri <%= zmq[:log] %> --error-uri <%= zmq[:error] %>
-=======
                      --notify-uri <%= zmq[:notify] %> --stats-uri <%= zmq[:stats] %>
                      --logs-uri <%= zmq[:logs] %> --error-uri <%= zmq[:error] %>
->>>>>>> Stashed changes
                      --router-uri <%= zmq[:router] %> --from-sink-uri <%= zmq[:from_sink] %>
 EOS
                    :resources => {
