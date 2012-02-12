507565078b13008c8b8aaf76979bc9869c31ef59
Noah Gibbs <noah@ooyala.com>
2012-02-11 22:11:46 -0800

    Add first-draft Cassandra sink for stats

---
+++ sinks/cass_stats/cass_sink.rb
@@ -0,0 +1,34 @@
+require "trollop"
+require "hastur/zmq_utils"
+require "hastur/message"
+require "hastur/sink/cassandra_schema"
+
+opts = Trollop::options do
+  banner "Creates a Cassandra keyspace\n\nOptions:"
+  opt :hosts,    "Cassandra Hostname(s)",  :default => ["127.0.0.1"],      :type => String, :multi => true
+  opt :hosts,    "Router URI(s)",          :default => ["127.0.0.1:4332"], :type => String, :multi => true
+  opt :keyspace, "Keyspace",               :default => "Hastur",           :type => String
+  opt :hwm,      "ZMQ message queue size", :default => 1,                  :type => :int
+end
+
+puts "Connecting to database at #{opts[:hosts][0]}:9160"
+client = Cassandra.new(opts[:keyspace], opts[:hosts].map { |h| "#{h}:9160" })
+
+client.default_write_consistency = 2  # Initial default: 1
+#client.default_read_consistency = 2
+
+socket = Hastur::ZMQUtils.connect_socket
+
+%w(INT TERM KILL).each do | sig |
+  Signal.trap(sig) do
+    @running = false
+    Signal.trap(sig, "DEFAULT")
+  end
+end
+
+while @running do
+  message = Hastur::Message.recv(socket)
+  Hastur::Cassandra.insert(message.payload)
+end
+
+STDERR.puts "Exited!"
