e6d4e31ac65d64ea21d810d29fc54b4acb864449
Viet Nguyen <viet@ooyala.com>
2012-02-13 18:57:57 -0800

    [java-client-api] Add stat unit tests.

--- client_apis/java/src/test/java/hastur/client/HasturApiTest.java
+++ client_apis/java/src/test/java/hastur/client/HasturApiTest.java
@@ -32,6 +32,52 @@ public class HasturApiTest {
   }
 
   @Test
+  public void testGauge() {
+    long currTime = System.nanoTime() / 1000;
+    boolean isSuccess = HasturApi.gauge("myLatency", currTime, 9.2, null);
+    try {
+      DatagramPacket msg = new DatagramPacket(new byte[65000], 65000);
+      server.receive(msg);
+      String rawMsg = new String(msg.getData());
+      JSONObject o = new JSONObject(rawMsg);
+      assertEquals("myLatency", o.get("name"));
+      assertEquals("stat", o.get("_route"));
+      assertEquals("gauge", o.get("type"));
+      assertEquals(currTime, o.get("timestamp"));
+      assertEquals(9.2, o.get("value"));
+      assertEquals("{}", o.get("labels"));
+    } catch(Exception e) {
+      e.printStackTrace();
+      assertTrue(false);
+    }
+    assertTrue(isSuccess);
+  }
+
+
+  @Test
+  public void testCounter() {
+    long currTime = System.nanoTime() / 1000; 
+    boolean isSuccess = HasturApi.counter("myLatency", currTime, 2, null);
+    try {
+      DatagramPacket msg = new DatagramPacket(new byte[65000], 65000);
+      server.receive(msg);
+      String rawMsg = new String(msg.getData());
+      JSONObject o = new JSONObject(rawMsg);
+      assertEquals("myLatency", o.get("name"));
+      assertEquals("stat", o.get("_route"));
+      assertEquals(currTime, o.get("timestamp"));
+      assertEquals(2, o.get("increment"));
+      assertEquals("counter", o.get("type"));
+      assertEquals("{}", o.get("labels"));
+    } catch(Exception e) {
+      e.printStackTrace();
+      assertTrue(false);
+    }
+    assertTrue(isSuccess);
+  }
+
+
+  @Test
   public void testMark() {
     long currTime = System.currentTimeMillis();
     boolean isSuccess = HasturApi.mark("myLatency", currTime, null);
