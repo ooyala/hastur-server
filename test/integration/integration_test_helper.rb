c72af1b0b39994f3c6ac2b142fe86dc22332ee2f
Al Tobey <al@ooyala.com>
2012-02-16 03:03:03 -0800

    [test helper] add some easy-to-spot UUID constants

--- test/integration/integration_test_helper.rb
+++ test/integration/integration_test_helper.rb
@@ -16,3 +16,11 @@ HASTUR_ROUTER_BIN="#{HASTUR_ROOT}/infrastructure/hastur-router.rb"
 HASTUR_CLIENT_BIN="#{HASTUR_ROOT}/bin/hastur-client-v2.rb"
 HASTUR_MSGTOOL_BIN="#{HASTUR_ROOT}/tools/msgtool.rb"
 
+# easy to spot fake UUID's
+C1UUID = '11111111-2222-3333-4444-555555555555'
+C2UUID = 'ffffffff-eeee-dddd-cccc-bbbbbbbbbbbb'
+C3UUID = '66666666-7777-8888-9999-aaaaaaaaaaaa'
+
+R1UUID = 'fafafafa-fafa-fafa-fafa-fafafafafafa'
+R2UUID = '01010101-0101-0101-0101-010101010101'
+
