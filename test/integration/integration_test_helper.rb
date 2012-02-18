ce6e91b09031127cbf0b77c65662a8962c26e363
Al Tobey <al@ooyala.com>
2012-02-18 14:24:42 -0800

    [test] add some test messages to test helper
    Not complete. Will fill out as they get used.

--- test/integration/integration_test_helper.rb
+++ test/integration/integration_test_helper.rb
@@ -24,3 +24,12 @@ C3UUID = '66666666-7777-8888-9999-aaaaaaaaaaaa'
 R1UUID = 'fafafafa-fafa-fafa-fafa-fafafafafafa'
 R2UUID = '01010101-0101-0101-0101-010101010101'
 
+MESSAGES = {
+  :heartbeat_client => "{\"last_heartbeat\":\"2012-02-18 12:29:48 -0800\",\"heartbeat\":30}",
+  :register_client  => "{\"uuid\":\"#{C1UUID}\",\"hostname\":\"web01.domain\",\"ipv4\":\"10.1.1.10\"}",
+  :notification     => "{\"sla\":604800,\"app\":\"sinatra\",\"recipients\":[\"web-oncall\"]}",
+  :stat             => "{\"name\":\"foo.bar.baz\",\"value\":1234,\"timestamp\":1234567890}",
+  :log              => "{}",
+  :error            => "{}",
+}
+
