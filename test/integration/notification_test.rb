79f95438aa386d726cb4b9a56e2affd28f8727e5
Al Tobey <al@ooyala.com>
2012-02-18 14:25:33 -0800

    [test] updates to notification_test.rb
    Still not working, but tracked down to a routing issue.
    
    A specific test for routes is coming in the next commit.

--- test/integration/notification_test.rb
+++ test/integration/notification_test.rb
@@ -17,23 +17,25 @@ class NotificationTest < Test::Unit::TestCase
     ack = proc do |messages|
       @ack_proc_calls += 1
       STDERR.puts "GOT A NOTIFICATION"
-      e = Hastur::Envelope.parse(messages[-2])
-      refute_nil e, "Hastur::Envelope.parse on messages[-2] must return an envelope."
-      assert e.ack? "Notifications must always have the ack flag enabled."
+      #e = Hastur::Envelope.parse(messages[-2])
+      #refute_nil e, "Hastur::Envelope.parse on messages[-2] must return an envelope."
+      #assert e.ack? "Notifications must always have the ack flag enabled."
     end
 
     @topology = Nodule::Topology.new(
       :greenio      => Nodule::Console.new(:fg => :green),
       :redio        => Nodule::Console.new(:fg => :red),
+      :cyanio       => Nodule::Console.new(:fg => :cyan),
       :client1unix  => Nodule::UnixSocket.new,
       :router       => Nodule::ZeroMQ.new(:uri => :gen),
       :heartbeat    => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :stderr),
       :register     => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :stderr),
-      #:notification => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :stderr),
-      :notification => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => ack, :limit => 4),
+      :notification => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :drain),
+      #:notification => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => ack, :limit => 4),
       :stat         => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :stderr),
       :log          => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :stderr),
-      :error        => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :stderr),
+      :error        => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :redio),
+      :plugin_exec  => Nodule::ZeroMQ.new(:connect => ZMQ::PUSH, :uri => :gen),
       :control      => Nodule::ZeroMQ.new(:connect => ZMQ::REQ,  :uri => :gen),
 
       :client1svc => Nodule::Process.new(
@@ -41,7 +43,7 @@ class NotificationTest < Test::Unit::TestCase
         '--router',       :router,
         '--unix',         :client1unix,
         '--ack-timeout',  1,
-        :stdout => :greenio, :stderr => :redio,
+        :stdout => :greenio, :stderr => :redio
       ),
 
       :routersvc => Nodule::Process.new(
@@ -49,13 +51,13 @@ class NotificationTest < Test::Unit::TestCase
         '--uuid',         R1UUID,
         '--heartbeat',    :heartbeat,
         '--register',     :register,
-        '--notification', :notify,
+        '--notification', :notification,
         '--stat',         :stat,
         '--log',          :log,
         '--error',        :error,
         '--router',       :router,
-        '--plugin-exec',  :from_sink,
-        :stdout => :greenio, :stderr => :redio,
+        '--plugin-exec',  :plugin_exec,
+        :stdout => :greenio, :stderr => :redio, :verbose => :cyanio
       ),
     )
 
@@ -83,12 +85,14 @@ EOJSON
 
     sleep 1
     puts "Sending notification..."
+
+    @topology[:client1unix].send notification
+    @topology[:client1unix].send notification
+    @topology[:client1unix].send notification
     @topology[:client1unix].send notification
 
-    @topology[:notification].wait
+    @topology[:notification].wait(5)
 
     assert 4 <= @ack_proc_calls, "The ack receiver proc should be called at least 4 times (got #{@ack_proc_calls})."
   end
-
 end
-
