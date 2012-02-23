e6f767cd78342a195eae387f68d3625f396fd81a
Viet Nguyen <viet@ooyala.com>
2012-02-22 22:30:55 -0800

    [stat-sink] Fix some things per CRs in the cassandra sink.

--- sinks/cassandra/cass_sink.rb
+++ sinks/cassandra/cass_sink.rb
@@ -1,7 +1,5 @@
 #!/usr/bin/env ruby
 
-$LOAD_PATH.unshift File.join(File.dirname(__FILE__), "..", "..", "lib")
-
 require "trollop"
 require "hastur/zmq_utils"
 require "hastur/message"
@@ -9,10 +7,12 @@ require "hastur/sink/cassandra_schema"
 
 opts = Trollop::options do
   banner "Creates a Cassandra keyspace\n\nOptions:"
-  opt :hosts,    "Cassandra Hostname(s)",  :default => ["127.0.0.1"],        :type => :strings, :multi => true
-  opt :routers,  "Router URI(s)",          :default => ["tcp://127.0.0.1:8127"], :type => :strings, :multi => true
-  opt :keyspace, "Keyspace",               :default => "Hastur",           :type => String
-  opt :hwm,      "ZMQ message queue size", :default => 1,                  :type => :int
+  opt :hosts,    "Cassandra Hostname(s)",  :default => ["127.0.0.1"],             :type => :strings,
+                                                                                  :multi => true
+  opt :routers,  "Router URI(s)",          :default => ["tcp://127.0.0.1:8127"],  :type => :strings,
+                                                                                  :multi => true
+  opt :keyspace, "Keyspace",               :default => "Hastur",                  :type => String
+  opt :hwm,      "ZMQ message queue size", :default => 1,                         :type => :int
 end
 
 puts "Connecting to database at #{opts[:hosts][0]}:9160"
@@ -20,7 +20,7 @@ client = Cassandra.new(opts[:keyspace], opts[:hosts].map { |h| "#{h}:9160" })
 
 client.default_write_consistency = 2  # Initial default: 1
 
-socket = Hastur::ZMQUtils.connect_socket(ZMQ::Context.new, ZMQ::PULL, opts[:routers][0])
+socket = Hastur::ZMQUtils.connect_socket(ZMQ::Context.new, ZMQ::PULL, opts[:routers])
 
 @running = true
 
