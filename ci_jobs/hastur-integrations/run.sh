34fcce21e1c13288a584ccb09e5a529f1c4ed6c6
Viet Nguyen <viet@ooyala.com>
2012-02-23 15:10:33 -0800

    [testing] update the jenkins run script to properly run hastur-server integration tests on jenkins.

--- ci_jobs/hastur-integrations/run.sh
+++ ci_jobs/hastur-integrations/run.sh
@@ -3,8 +3,8 @@
 : ${REPO_ROOT:="$WORKSPACE"}
 source $HOME/.rvm/scripts/rvm
 
-cd $REPO_ROOT/hastur
-rvm --create use 1.9.2@hastur
+cd $REPO_ROOT/hastur-server
+rvm --create use 1.9.2@hastur-server
 gem install --no-rdoc --no-ri bundler
 bundle install
 rake --trace test:integrations
