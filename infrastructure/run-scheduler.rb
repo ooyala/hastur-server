d1140ff00050f84a26f455eeffd8c675b6099ba8
Viet Nguyen <viet@ooyala.com>
2012-02-28 14:29:52 -0800

    [scheduler] Wrap trollop around the run-scheduler script.

--- infrastructure/run-scheduler.rb
+++ infrastructure/run-scheduler.rb
@@ -1,9 +1,28 @@
+#!/usr/bin/env ruby
+
 require "hastur-heap-scheduler"
+require "hastur-server/zmq_utils"
+require "trollop"
+
+opts = Trollop::options do
+  opt :router, "ZMQ URI for the router", :default => "tcp://127.0.0.1:8126", :type => String
+end
+
+if opts[:router] !~ /\w+:\/\/[^:]+:\d+/
+  Trollop::die :router, "Option --router must be of the form protocol://hostname:port"
+end
+
+if ZMQ::LibZMQ.version2? && opts[:router] =~ /\Wlocalhost\W/
+  Trollop::die :router, "Don't use 'localhost'. ZMQ 2.x will break silently around IPv6 localhost."
+end
 
 # TODO(viet): For now, test the scheduler with just a flat file
 TEMP_JOB_FILE=File.join(File.dirname(__FILE__), "..", "test", "data", "plugin.json")
 
-scheduler = Hastur::Scheduler.new
+ctx = ZMQ::Context.new(1)
+router_socket = Hastur::ZMQUtils.connect_socket(ctx, ZMQ::PUSH, opts[:router])
+
+scheduler = Hastur::Scheduler.new(router_socket)
 scheduler.start
 
 # read scheduled jobs
