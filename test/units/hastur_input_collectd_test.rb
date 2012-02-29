2f5b56d2366682680d4d4b2126ffb87e661b52c6
Al Tobey <al@ooyala.com>
2012-02-28 23:32:31 -0800

    [test] add skip to hastur_input_collectd_test.rb
    pack/unpack of Q> doesn't work on Ruby < 1.9.3

--- test/units/hastur_input_collectd_test.rb
+++ test/units/hastur_input_collectd_test.rb
@@ -9,40 +9,45 @@ require 'hastur-server/input/collectd'
 PACKET_DIR = File.join(File.dirname(__FILE__), '..', 'data', 'collectd-raw-packets')
 
 class TestHasturInputCollectd < MiniTest::Unit::TestCase
-
   # ruby hastur_collectd_test.rb --name test_custom_full_packet
   def test_custom_full_packet
+    # test for Q> support (1.9.3 is required)
+    vbin = [123].pack("Q>").unpack("NN")
+    if ((vbin[0] << 32) + vbin[1]).to_i != 123
+      skip "This test is only supported on Ruby 1.9.3 and above."
+    end
+
     values = [
       4,                                         # n
       Hastur::Input::Collectd::DS_TYPE_COUNTER,  # C
       Hastur::Input::Collectd::DS_TYPE_GAUGE,    # C
       Hastur::Input::Collectd::DS_TYPE_DERIVE,   # C
       Hastur::Input::Collectd::DS_TYPE_ABSOLUTE, # C
-      9876543210,                                # CQ>
-      3.14159265358979323846,                    # CE
-      -111111111,                                # Cq>
-      1234567890,                                # CQ>
+      9876543210,                                # Q>
+      3.14159265358979323846,                    # E
+      -111111111,                                # q>
+      1234567890,                                # Q>
     ]
 
     vals = values.pack( %w[n C4 Q> E q> Q>].join )
 
     data = [
       Hastur::Input::Collectd::TYPE_HOST,            11, "abcdef",        # nnZ7 (4 + strlen + 1)
-      Hastur::Input::Collectd::TYPE_TIME,            12, 123,             # nnQ uint64_t
-      Hastur::Input::Collectd::TYPE_TIME_HR,         12, 321,             # nnQ uint64_t
+      Hastur::Input::Collectd::TYPE_TIME,            12, 123,             # nnQ> uint64_t
+      Hastur::Input::Collectd::TYPE_TIME_HR,         12, 321,             # nnQ> uint64_t
       Hastur::Input::Collectd::TYPE_PLUGIN,          11, "foobar",        # nnZ7  (4 + strlen + 1)
       Hastur::Input::Collectd::TYPE_PLUGIN_INSTANCE, 8,  "b z",           # nnZ4  (4 + strlen + 1)
       Hastur::Input::Collectd::TYPE_TYPE,            6,  "1",             # nnZ2  (4 + strlen + 1)
       Hastur::Input::Collectd::TYPE_TYPE_INSTANCE,   12, "abcdefg",       # nnZ8  (4 + strlen + 1)
-      Hastur::Input::Collectd::TYPE_INTERVAL,        12, 456,             # nnQ uint64_t
-      Hastur::Input::Collectd::TYPE_INTERVAL_HR,     12, 654,             # nnQ uint64_t
+      Hastur::Input::Collectd::TYPE_INTERVAL,        12, 456,             # nnQ> uint64_t
+      Hastur::Input::Collectd::TYPE_INTERVAL_HR,     12, 654,             # nnQ> uint64_t
       Hastur::Input::Collectd::TYPE_MESSAGE,         10, "help!",         # nnZ6  (4 + strlen + 1)
-      Hastur::Input::Collectd::TYPE_SEVERITY,        12, 789,             # nnQ uint64_t
+      Hastur::Input::Collectd::TYPE_SEVERITY,        12, 789,             # nnQ> uint64_t
       Hastur::Input::Collectd::TYPE_VALUES,       vals.bytesize + 4, vals # nna*
     ]
 
     packet = data.pack(
-      %w[nnZ7 nnQ nnQ nnZ7 nnZ4 nnZ2 nnZ8 nnQ nnQ nnZ6 nnQ nna*].join
+      %w[nnZ7 nnQ> nnQ> nnZ7 nnZ4 nnZ2 nnZ8 nnQ> nnQ> nnZ6 nnQ> nna*].join
     )
 
     stat = Hastur::Input::Collectd.decode_packet(packet)
