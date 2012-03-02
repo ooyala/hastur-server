fee571d4b1a7077f984230e3454bfc9890d68f2d
Noah Gibbs <noah@ooyala.com>
2012-03-02 13:22:03 -0800

    Cassandra: add first draft of some rollup logic

---
+++ lib/hastur-server/sink/cassandra_rollups.rb
@@ -0,0 +1,117 @@
+require "hastur-server/sink/cassandra_schema"
+
+module Hastur
+  module Cassandra
+    extend self
+
+    NAME_BY_GRANULARITY = {
+      FIVE_MINUTES => "FiveMinute",
+      ONE_HOUR => "Hour",
+      ONE_DAY => "Day",
+      ONE_WEEK => "Week",
+      # Month and year are weird - they aren't the same kind of simple granularities
+    }
+
+    # For now, leave out month and year to keep the math simple
+    GRANULARITIES = [ONE_WEEK, ONE_DAY, ONE_HOUR, FIVE_MINUTES]
+
+    #
+    # UNTESTED
+    #
+    # For messages with multi-granularity rollups (i.e. hours, days,
+    # weeks) we need to be able to take a chunk of time and divide it
+    # into pieces of granularity sizes.  For instance, a nine-day span
+    # might turn into a single one-week chunk and two one-day chunks
+    # on either side.
+    #
+    # @param [Fixnum] start_ts The beginning timestamp of the time interval
+    # @param [Fixnum] end_ts The end timestamp of the time interval
+    # @param [Array] granularities The list of granularities to break the interval into
+    #
+    def get_granular_segments_from_timestamps(start_ts, end_ts, granularities = GRANULARITIES)
+      return [] if granularities.empty?
+      return [] if (end_ts - start_ts) > granularities[0]
+
+      # Start with the largest granularity - a week, at the top level
+      granularity = granularities[0]
+
+      # Get the first whole chunk (e.g. week) that the first timestamp overlaps
+      first_segment = time_segment_for_timestamp(start_ts, granularity)
+
+      # Now, get the first whole chunk that starts *after* start_ts
+      first_segment += granularity if start_ts > first_segment
+
+      # Now, how many whole chunks this size can we go forward before we're done?
+      segments = time_segments_for_timestamps(first_segment, end_ts, granularity)
+
+      # Remove the last chunk if it extends past the end timestamp
+      segments.pop if (segments[-1] + granularity - 1) > end_ts
+
+      # Now we have the whole list of all biggest-size chunks wholly contained in the times.
+      # Convert to intervals so it's clearer what's going on and we know the granularities.
+      segments = segments.map { |s| [ s, s + granularity - 1] }
+
+      # Now, grab all smaller chunks before the start of the first big chunk, and all smaller
+      # chunks after the end of the last big chunk.  Then return everything.
+      get_time_segments_from_timestamps(start_ts, segments[0][0] - 1, granularities[1..-1]) +
+        segments +
+        get_time_segments_from_timestamps(segments[-1][1] + 1, segments[0] * 1_000_000, granularities[1..-1])
+    end
+
+    #
+    # Get the list of cassandra queries to find this time range of UUIDs
+    #
+    def get_uuid_cass_queries_over_time(start_ts, end_ts, options = {})
+      # TODO(noah) - consider expanding opportunistically to return larger-granularity buckets instead
+      # of multiple smaller ones.  UUIDs in particular will have a lot of repetition in the small
+      # buckets.
+
+      # Make sure we return all relevant UUIDs - for now, just expand five minutes in either direction
+      segments = get_granular_segments_from_timestamps(start_ts - FIVE_MINUTES, end_ts + FIVE_MINUTES)
+
+      cass_queries = segments.map do |seg_start_ts, seg_end_ts|
+        granularity = seg_end_ts - seg_start_ts + 1
+        gran_name = NAME_BY_GRANULARITY[granularity]
+        raise "Unknown granularity #{granularity}!" unless gran_name
+
+        [ "UUID#{gran_name}".to_sym, start_ts.to_s ]
+      end
+    end
+
+    #
+    # Get the list of cassandra queries to find this time range of stat names.
+    #
+    def get_stat_name_cass_queries_over_time(start_ts, end_ts, options = {})
+      # Make sure we return all relevant stat names - for now, just expand five minutes in either direction
+      segments = get_granular_segments_from_timestamps(start_ts - FIVE_MINUTES, end_ts + FIVE_MINUTES)
+
+      cass_queries = segments.map do |seg_start_ts, seg_end_ts|
+        granularity = seg_end_ts - seg_start_ts + 1
+        gran_name = NAME_BY_GRANULARITY[granularity]
+        raise "Unknown granularity #{granularity}!" unless gran_name
+
+        [ "StatNames#{gran_name}".to_sym, start_ts.to_s ]
+      end
+    end
+
+    #
+    # Convert a cassandra query list to the actual data from the queries.
+    #
+    def cass_queries_to_data(cass_client, queries, options = { :count => 10_000, :consistency => 1 })
+      queries_by_cf = {}
+
+      queries.each do |cf, row_key|
+        queries_by_cf[cf] ||= []
+        queries_by_cf[cf] << row_key
+      end
+
+      values = {}
+      queries_by_cf.each do |cf, rows|
+        values.merge! cass_client.multi_get(cf, rows, options)
+      end
+
+      values
+    end
+
+  end
+end
---
+++ test/units/sinks/cassandra_rollups_test.rb
@@ -0,0 +1,56 @@
+require_relative "../../test_helper"
+
+require "hastur-server/sink/cassandra_rollups"
+
+# Timestamps to test with
+TESTING_NOW_TS = 1330718485000000
+
+# Tues, March 6th
+TUESDAY_TS = Time.utc(2012, 3, 6).to_i * 1_000_000
+WEDNESDAY_TS = Time.utc(2012, 3, 7).to_i * 1_000_000
+THURSDAY_TS = Time.utc(2012, 3, 8).to_i * 1_000_000
+FRIDAY_TS = Time.utc(2012, 3, 9).to_i * 1_000_000
+SATURDAY_TS = Time.utc(2012, 3, 10).to_i * 1_000_000
+
+# Sun, March 11th
+NEXT_SUNDAY_TS = Time.utc(2012, 3, 11).to_i * 1_000_000
+
+# Sun, March 18th
+NEXT_NEXT_SUNDAY_TS = Time.utc(2012, 3, 18).to_i * 1_000_000
+# Mon, March 19th
+NEXT_NEXT_MONDAY_TS = Time.utc(2012, 3, 20).to_i * 1_000_000
+# Tues, March 20th
+NEXT_NEXT_TUESDAY_TS = Time.utc(2012, 3, 20).to_i * 1_000_000
+
+# These are granularities and so are in seconds, not usec.
+# Hm...  Maybe need to fix that :-/
+ONE_HOUR = 60 * 60
+ONE_DAY = 24 * ONE_HOUR
+ONE_WEEK = 7 * ONE_DAY
+
+class CassandraRollupsTest < Scope::TestCase
+  setup do
+    @cass_client = mock("Cassandra client")
+    @cass_client.stubs(:batch).yields(@cass_client)
+    Hastur::Util.stubs(:timestamp).with(nil).returns(TESTING_NOW_TS)
+  end
+
+=begin
+  context "with rollup segments" do
+    should "..." do
+      segs = Hastur::Cassandra.get_granular_segments_from_timestamps(TUESDAY_TS,
+                                        NEXT_NEXT_TUESDAY_TS, [ ONE_WEEK, ONE_DAY ])
+      assert_equal [
+                    [TUESDAY_TS, WEDNESDAY_TS - 1],
+                    [WEDNESDAY_TS, THURSDAY_TS - 1],
+                    [THURSDAY_TS, FRIDAY_TS - 1],
+                    [FRIDAY_TS, SATURDAY_TS - 1],
+                    [SATURDAY_TS, NEXT_SUNDAY_TS - 1],
+                    [NEXT_SUNDAY_TS, NEXT_NEXT_SUNDAY_TS - 1],
+                    [NEXT_NEXT_SUNDAY_TS, NEXT_NEXT_MONDAY_TS - 1],
+                    [NEXT_NEXT_MONDAY_TS, NEXT_NEXT_TUESDAY_TS - 1],
+                   ], segs
+    end
+  end
+=end
+end
