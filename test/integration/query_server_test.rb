733c160b94e03fc878a3d1734eb310ef1b5c21ee
Noah Gibbs <noah@ooyala.com>
2012-03-06 10:25:09 -0800

    Initial query server integration test without real end-to-end checking

---
+++ test/integration/query_server_test.rb
@@ -0,0 +1,89 @@
+#!/usr/bin/env ruby
+
+require "rubygems"
+require "test/unit"
+require_relative "./integration_test_helper"
+
+require 'nodule/topology'
+require 'nodule/process'
+require 'nodule/console'
+require 'nodule/unixsocket'
+require 'nodule/zeromq'
+require 'multi_json'
+
+require 'hastur'
+
+class QueryServerTest < Test::Unit::TestCase
+  def setup
+    @topology = Nodule::Topology.new(
+      :greenio      => Nodule::Console.new(:fg => :green),
+      :redio        => Nodule::Console.new(:fg => :red),
+      :client1unix  => Nodule::UnixSocket.new,
+      :client2unix  => Nodule::UnixSocket.new,
+      :router       => Nodule::ZeroMQ.new(:uri => :gen),
+      :heartbeat    => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :capture, :limit => 4),
+      :registration => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :drain),
+      :stat         => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :drain),
+      :event        => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :drain),
+      :log          => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :drain),
+      :error        => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :drain),
+      :control      => Nodule::ZeroMQ.new(:connect => ZMQ::REQ,  :uri => :gen),
+      :direct       => Nodule::ZeroMQ.new(:connect => ZMQ::PUSH, :uri => :gen),
+
+      :cassandra    => Nodule::Process.new("#{ENV['HOME']}/apache-cassandra-1.0.7/bin/cassandra", "-f"),
+      :query_server => Nodule::Process.new(HASTUR_QUERY_SERVER_BIN, "--", "-p", "4177"),
+
+      :client1svc   => Nodule::Process.new(
+        HASTUR_CLIENT_BIN, '--uuid', C1UUID, '--heartbeat', 1, '--router', :router, '--unix', :client1unix,
+        :stdout => :greenio, :stderr => :redio,
+      ),
+
+      :client2svc => Nodule::Process.new(
+        HASTUR_CLIENT_BIN, '--uuid', C2UUID, '--heartbeat', 1, '--router', :router, '--unix', :client2unix,
+        :stdout => :greenio, :stderr => :redio,
+      ),
+
+      :routersvc => Nodule::Process.new(
+        HASTUR_ROUTER_BIN,
+        '--uuid',         R1UUID,
+        '--heartbeat',    :heartbeat,
+        '--registration', :registration,
+        '--event',        :event,
+        '--stat',         :stat,
+        '--log',          :log,
+        '--error',        :error,
+        '--router',       :router,
+        '--direct',       :direct,
+        :stdout => :greenio, :stderr => :redio, :verbose => :cyanio
+      ),
+    )
+
+    @topology.start_all
+  end
+
+  def teardown
+    @topology.stop_all
+  end
+
+  def test_query_server
+    # wait for heartbeats to flow through and get into Cassandra
+    sleep 3
+
+    messages = @topology[:heartbeat].output
+    # First, check messages
+    payloads  = messages.map { |m| MultiJson.decode(m[-1]) }
+    envelopes = messages.map { |m| m[-2].unpack("H*") }
+
+    assert_equal 4, messages.count, "Should have exactly two captured messages"
+
+    # Query from 10 minutes ago to 10 minutes from now, just to grab everything
+    start_ts = Hastur.timestamp(Time.now.to_i - 600)
+    end_ts = Hastur.timestamp(Time.now.to_i + 600)
+
+    c1_messages = MultiJson.decode `curl http://localhost:4177/data/heartbeat/json?uuid=#{C1UUID}\&start=#{start_ts}\&end=#{end_ts}`
+    c2_messages = MultiJson.decode `curl http://localhost:4177/data/heartbeat/values?uuid=#{C2UUID}\&start=#{start_ts}\&end=#{end_ts}`
+
+    STDERR.puts "Client 1 messages: #{c1_messages.inspect}"
+    STDERR.puts "Client 2 messages: #{c2_messages.inspect}"
+  end
+end
