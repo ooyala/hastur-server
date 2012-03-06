d8f3f3613d2f84d3092a3cac99137f6858e289e3
Noah Gibbs <noah@ooyala.com>
2012-03-06 08:51:52 -0800

    Add HASTUR_QUERY_SERVER_BIN path

--- test/integration/integration_test_helper.rb
+++ test/integration/integration_test_helper.rb
@@ -16,6 +16,7 @@ HASTUR_ROOT = File.join(File.dirname(__FILE__), "..", "..")
 HASTUR_ROUTER_BIN="#{HASTUR_ROOT}/infrastructure/hastur-router.rb"
 HASTUR_CLIENT_BIN="#{HASTUR_ROOT}/bin/hastur-client.rb"
 HASTUR_MSGTOOL_BIN="#{HASTUR_ROOT}/tools/msgtool.rb"
+HASTUR_QUERY_SERVER_BIN="#{HASTUR_ROOT}/infrastructure/hastur-query-server.rb"
 
 # easy to spot fake UUID's
 C1UUID = '11111111-2222-3333-4444-555555555555'
