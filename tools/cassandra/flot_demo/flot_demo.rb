f8f62fbf6b7ac84c8a16d2280c4fb33768ac1fbe
Noah Gibbs <noah@ooyala.com>
2012-03-08 10:24:35 -0800

    Flot demo: pass error codes on to JS through proxy

--- tools/cassandra/flot_demo/flot_demo.rb
+++ tools/cassandra/flot_demo/flot_demo.rb
@@ -3,8 +3,21 @@
 require "sinatra"
 require "httparty"
 
+get "/" do
+  redirect "/index.html"
+end
+
+get "/uuids_proxy" do
+  url = "http://localhost:4177/uuids?start=#{params[:start]}&end=#{params[:end]}"
+
+  response = HTTParty.get(url)
+  [ response.code, response.body ]
+end
+
 get "/data_proxy/*" do
   url = "http://localhost:4177/data/stat/values?start=#{params[:start]}&end=#{params[:end]}&uuid=#{params[:uuid]}"
   url += "&name=#{params[:name]}" if params[:name]
-  [ 200, HTTParty.get(url).body ]
+
+  response = HTTParty.get(url)
+  [ response.code, response.body ]
 end
