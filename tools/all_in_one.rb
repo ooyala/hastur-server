30d5c69c4c52040521fb3d1da217430e5126f142
Al Tobey <al@ooyala.com>
2012-03-12 11:48:59 -0700

    Update all_in_one.rb to match what I do on my workstation.
    Brings up all of Hastur for testing on a single machine.

--- tools/all_in_one.rb
+++ tools/all_in_one.rb
@@ -6,70 +6,39 @@ require 'nodule/unixsocket'
 require 'nodule/zeromq'
 require 'nodule/console'
 
-require 'hastur-server/message'
 require 'multi_json'
 require 'rainbow'
 
 uuid1 = '11111111-2222-3333-4444-555555555555'
 uuid2 = 'ffffffff-eeee-dddd-cccc-bbbbbbbbbbbb'
 
-hastur_msg = proc do |messages|
-  begin
-    m = Hastur::Message.parse(messages[-2], messages[-1])
-
-    case m
-      when Hastur::Message::Stat
-        stat = m.decode
-        value = stat[:value] or stat[:increment]
-        puts "#{stat[:name].to_s.ljust(70)} #{stat[:type].ljust(8)} #{value.to_s.ljust(20)} #{stat[:timestamp]}".color(:blue)
-
-      # TODO: print out other message types
-      when Hastur::Message::Log
-      when Hastur::Message::Error
-      when Hastur::Message::Rawdata
-      when Hastur::Message::Notification
-      when Hastur::Message::HeartbeatClient
-      when Hastur::Message::HeartbeatService
-      when Hastur::Message::PluginExec
-      when Hastur::Message::PluginResult
-      when Hastur::Message::RegisterClient
-      when Hastur::Message::RegisterPlugin
-      when Hastur::Message::RegisterService
-      else
-        raise "Unrecognized message type!?!?"
-    end
-  rescue Exception => e
-    STDERR.puts "Error parsing Hastur message: #{e}".color(:red)
-  end
-end
-
 topology = Nodule::Topology.new(
   :cyanio        => Nodule::Console.new(:fg => :cyan),
+  :greenio       => Nodule::Console.new(:fg => :green),
+  :yellowio      => Nodule::Console.new(:fg => :yellow),
+  :redio         => Nodule::Console.new(:fg => :red),
   :router        => Nodule::ZeroMQ.new(:uri => :gen),
-  :notification  => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => hastur_msg),
-  :heartbeat     => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => hastur_msg),
-  :register      => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => hastur_msg),
-  :stat          => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => hastur_msg),
-  :log           => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => hastur_msg),
-  :error         => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => hastur_msg),
-  :rawdata       => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => hastur_msg),
-  :plugin_result => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => hastur_msg),
-  :plugin_exec   => Nodule::ZeroMQ.new(:connect => ZMQ::PUSH, :uri => :gen),
-  :acks          => Nodule::ZeroMQ.new(:connect => ZMQ::PUSH, :uri => :gen),
+  :stat          => Nodule::ZeroMQ.new(:uri => :gen),
+  :event         => Nodule::ZeroMQ.new(:uri => :gen),
+  :heartbeat     => Nodule::ZeroMQ.new(:uri => :gen),
+  :registration  => Nodule::ZeroMQ.new(:uri => :gen),
+  :log           => Nodule::ZeroMQ.new(:uri => :gen),
+  :error         => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :redio),
+  :rawdata       => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :greenio),
+  :direct        => Nodule::ZeroMQ.new(:connect => ZMQ::PUSH, :uri => :gen),
   :control       => Nodule::ZeroMQ.new(:connect => ZMQ::REQ,  :uri => :gen),
   :routersvc     => Nodule::Process.new(
     '../infrastructure/hastur-router.rb',
     '--uuid',          uuid1,
+    '--hwm',           100,
     '--router',        :router,
-    '--notification',  :notification,
+    '--event',         :event,
     '--heartbeat',     :heartbeat,
-    '--register',      :register,
+    '--registration',  :registration,
     '--stat',          :stat,
     '--log',           :log,
     '--error',         :error,
-    '--plugin-exec',   :plugin_exec,
-    '--plugin-result', :plugin_result,
-    '--acks',          :acks,
+    '--direct',        :direct,
     '--rawdata',       :rawdata,
     '--control',       :control,
     :stdout => :cyanio, :stderr => :cyanio, :verbose => :cyanio,
@@ -81,6 +50,10 @@ topology = Nodule::Topology.new(
     '--port',         8125,
     '--ack-timeout',  5,
     :stdout => :cyanio, :stderr => :cyanio, :verbose => :cyanio,
+  ),
+  :cass_sink => Nodule::Process.new(
+    '../bin/cass_sink.rb', '--routers', :stat, :event, :heartbeat, :registration, :log,
+    :verbose => :cyanio, :stderr => :redio, :stdout => :yellowio
   ),
 )
 
