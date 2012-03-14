60bca286d2b791d287bc17301200e9705d0ec598
Jay Bhat <jbhat@ooyala.com>
2012-03-14 16:18:30 -0700

    update start script

---
+++ tools/start_everything.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../bin/hastur-client.rb --router tcp://127.0.0.1:4321 --port 8125 --uuid 11111111-2222-3333-4444-555555555555
--- tools/start_router_n_client.sh
+++ tools/start_router_n_client.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-../infrastructure/hastur-router.rb &
-../bin/hastur-client.rb --router tcp://127.0.0.1:4321 --port 8125 --uuid thisismyuuid
+../bin/hastur-router.rb --uuid 11111111-2222-3333-4444-555555555555 &
+../bin/hastur-client.rb --router tcp://127.0.0.1:4321 --port 8125 --uuid ffffffff-eeee-dddd-cccc-bbbbbbbbbbbb
