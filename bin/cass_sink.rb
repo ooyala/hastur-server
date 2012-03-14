acfe571a58cb0e46d1b091600e6904b6e79b0af5
Noah Gibbs <noah@ooyala.com>
2012-03-14 13:37:29 -0700

    First draft patching of cass_sink to allow nonstandard port numbers, multiple routers, multiple sink port numbers.

--- bin/cass_sink.rb
+++ bin/cass_sink.rb
@@ -6,19 +6,32 @@ require "hastur-server/sink/cassandra_schema"
 
 opts = Trollop::options do
   banner "Creates a Cassandra keyspace\n\nOptions:"
-  opt :hosts,    "Cassandra Hostname(s)",  :default => ["127.0.0.1"],             :type => :strings,
-                                                                                  :multi => true
-  opt :routers,  "Router URI(s)",          :default => ["tcp://127.0.0.1:8127"],  :type => :strings,
-                                                                                  :multi => true
-  opt :keyspace, "Keyspace",               :default => "Hastur",                  :type => String
-  opt :hwm,      "ZMQ message queue size", :default => 1,                         :type => :int
+  opt :hosts,     "Cassandra URI(s)",       :default => ["127.0.0.1:9160"],        :type => :strings,
+                                                                                   :multi => true
+  opt :routers,   "Router URI(s)",          :default => ["tcp://127.0.0.1"],       :type => :strings,
+                                                                                   :multi => true
+  opt :sink_port, "Router sink port num(s)",:default => 8127,                      :type => :integer,
+                                                                                   :multi => true
+  opt :ack_port,  "Router ack port num",    :default => 8134,                      :type => :integer
+  opt :keyspace,  "Keyspace",               :default => "Hastur",                  :type => String
+  opt :hwm,       "ZMQ message queue size", :default => 1,                         :type => :int
 end
 
 ctx = ZMQ::Context.new
-socket = Hastur::ZMQUtils.connect_socket(ctx, ZMQ::PULL, opts[:routers].flatten)
 
-puts "Connecting to database at #{opts[:hosts][0]}:9160"
-client = Cassandra.new(opts[:keyspace], opts[:hosts].map { |h| "#{h}:9160" })
+# Build a list of URIs that are the direct port on each router
+msg_uri_list = opts[:routers].flatten.map { |r| "#{r}:#{opts[:ack_port]}" }
+
+# Build a list of URIs of routers cross sink ports.
+# So if there are six routers and we're being a sink for four message types,
+#   you'd get 24 URIs to connect to.  Load balancing!
+ack_uri_list = opts[:routers].flatten.map { |r| [opts[:sink_port]].flatten.map { |p| "#{r}:#{p}" }}.flatten
+
+msg_socket = Hastur::ZMQUtils.connect_socket(ctx, ::ZMQ::PULL, msg_uri_list)
+ack_socket = Hastur::ZMQUtils.connect_socket(ctx, ::ZMQ::PUSH, ack_uri_list)
+
+puts "Connecting to Cassandra at #{opts[:hosts].inspect}"
+client = Cassandra.new(opts[:keyspace], opts[:hosts])
 client.default_write_consistency = 2  # Initial default: 1
 
 @running = true
@@ -32,11 +45,13 @@ end
 while @running do
   begin
     puts "Receiving..."
-    message = Hastur::Message.recv(socket)
+    message = Hastur::Message.recv(msg_socket)
+    envelope = message.envelope
     uuid = message.envelope.from
     route = message.type_symbol.to_s
     puts "[#{route}] - #{message.payload}"
     Hastur::Cassandra.insert(client, message.payload, route, :uuid => uuid)
+    envelope.to_ack.send(ack_socket) if envelope.ack?
   rescue Exception => e
     puts e.message
     puts e.backtrace
