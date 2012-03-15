098a66e691eb1d0653ece1502db72bb9668c4031
Jay Bhat <jbhat@ooyala.com>
2012-03-15 14:22:29 -0700

    get bring up test working sans sinks for now

--- test/integration/bring_up_test.rb
+++ test/integration/bring_up_test.rb
@@ -16,38 +16,36 @@ class BringUpTest < Test::Unit::TestCase
     @topology = Nodule::Topology.new(
       :greenio      => Nodule::Console.new(:fg => :green),
       :redio        => Nodule::Console.new(:fg => :red),
-      :cyanio       => Nodule::Console.new(:fg => :cyan),
       :yellowio     => Nodule::Console.new(:fg => :yellow),
+      :cyanio       => Nodule::Console.new(:fg => :cyan),
       :client1unix  => Nodule::UnixSocket.new,
       :client2unix  => Nodule::UnixSocket.new,
       :router       => Nodule::ZeroMQ.new(:uri => :gen),
       :heartbeat    => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :capture),
       :registration => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :drain),
-      :stat         => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen),
+      :stat         => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :drain),
       :event        => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :drain),
       :log          => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :drain),
       :error        => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :drain),
-      :control      => Nodule::ZeroMQ.new(:connect => ZMQ::REQ,  :uri => :gen),
+      :rawdata      => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :drain),
+      :control      => Nodule::ZeroMQ.new(:connect => ZMQ::REP,  :uri => :gen),
       :direct       => Nodule::ZeroMQ.new(:connect => ZMQ::PUSH, :uri => :gen),
-      :cassandra    => Nodule::Cassandra.new(:keyspace => "Hastur"),
-      :query_server => Nodule::Process.new(HASTUR_QUERY_SERVER_BIN,
-        '--cassandra', :cassandra,
-        '--', '-p', '4177',
-        :stdout => :greenio, :stderr => proc do |line|
-          sinatra_ready = true if line =~ /== Sinatra.* has taken the stage/
-        end,
-      ),
-
+      # :cassandra    => Nodule::Cassandra.new(:keyspace => "Hastur"),
+      # :query_server => Nodule::Process.new(HASTUR_QUERY_SERVER_BIN,
+      #   '--cassandra', :cassandra,
+      #   '--', '-p', '4177',
+      #   :stdout => :greenio, :stderr => proc do |line|
+      #     sinatra_ready = true if line =~ /== Sinatra.* has taken the stage/
+      #   end,
+      # ),
       :client1svc   => Nodule::Process.new(
         HASTUR_CLIENT_BIN, '--uuid', C1UUID, '--heartbeat', 1, '--router', :router, '--unix', :client1unix,
-        :stdout => :greenio, :stderr => :redio,
+        :stdout => :greenio, :stderr => :redio, :verbose => :yellowio,
       ),
-
       :client2svc => Nodule::Process.new(
         HASTUR_CLIENT_BIN, '--uuid', C2UUID, '--heartbeat', 1, '--router', :router, '--unix', :client2unix,
-        :stdout => :greenio, :stderr => :redio,
+        :stdout => :greenio, :stderr => :redio, :verbose => :yellowio,
       ),
-
       :routersvc => Nodule::Process.new(
         HASTUR_ROUTER_BIN,
         '--uuid',         R1UUID,
@@ -57,28 +55,30 @@ class BringUpTest < Test::Unit::TestCase
         '--stat',         :stat,
         '--log',          :log,
         '--error',        :error,
+        '--rawdata',      :rawdata,
+        '--control',      :control,
         '--router',       :router,
         '--direct',       :direct,
         '--hwm',          10,   # Set HWM so this doesn't 'clog'
         :stdout => :greenio, :stderr => :redio, :verbose => :cyanio
       ),
-      :cass_sink => Nodule::Process.new(
-        HASTUR_CASS_SINK_BIN,
-        '--sinks', :stat, :event, :heartbeat, :registration, :log, :error,
-        '--cassandra', :cassandra,
-        :verbose => :cyanio, :stderr => :redio, :stdout => :yellowio
-      ),
+      # :cass_sink => Nodule::Process.new(
+      #   HASTUR_CASS_SINK_BIN,
+      #   '--sinks', :stat, :event, :heartbeat, :registration, :log, :error,
+      #   '--cassandra', :cassandra,
+      #   :verbose => :cyanio, :stderr => :redio, :stdout => :yellowio
+      # ),
     )
 
-    @topology.start :cassandra
-    create_all_column_families(@topology[:cassandra]) # helper
+    # @topology.start :cassandra
+    # create_all_column_families(@topology[:cassandra]) # helper
 
     @topology.start_all
-    sleep 0.01 until sinatra_ready
+    # sleep 0.01 until sinatra_ready
   end
 
   def teardown
-    @topology.stop :cass_sink
+    # @topology.stop :cass_sink
     @topology.stop_all
   end
 
@@ -95,13 +95,13 @@ class BringUpTest < Test::Unit::TestCase
     STDERR.puts "Heartbeat message(s): #{messages.inspect}"
 
     # Query from 10 minutes ago to 10 minutes from now, just to grab everything
-    start_ts = Hastur.timestamp(Time.now.to_i - 600)
-    end_ts = Hastur.timestamp(Time.now.to_i + 600)
-
-    c1_messages = MultiJson.decode `curl http://localhost:4177/data/heartbeat/json?uuid=#{C1UUID}\\\&start=#{start_ts}\\\&end=#{end_ts}`
-    c2_messages = MultiJson.decode `curl http://localhost:4177/data/heartbeat/values?uuid=#{C2UUID}\\\&start=#{start_ts}\\\&end=#{end_ts}`
-
-    STDERR.puts "Client 1 messages: #{c1_messages.inspect}"
-    STDERR.puts "Client 2 messages: #{c2_messages.inspect}"
+    # start_ts = Hastur.timestamp(Time.now.to_i - 600)
+    # end_ts = Hastur.timestamp(Time.now.to_i + 600)
+    #
+    # c1_messages = MultiJson.decode `curl http://localhost:4177/data/heartbeat/json?uuid=#{C1UUID}\\\&start=#{start_ts}\\\&end=#{end_ts}`
+    # c2_messages = MultiJson.decode `curl http://localhost:4177/data/heartbeat/values?uuid=#{C2UUID}\\\&start=#{start_ts}\\\&end=#{end_ts}`
+    #
+    # STDERR.puts "Client 1 messages: #{c1_messages.inspect}"
+    # STDERR.puts "Client 2 messages: #{c2_messages.inspect}"
   end
 end
