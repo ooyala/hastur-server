d6cc199987008a644a25312752bcec7e1e462510
Noah Gibbs <noah@ooyala.com>
2012-03-15 12:48:41 -0700

    Change 'route' to 'type' in query server code

--- bin/hastur-query-server.rb
+++ bin/hastur-query-server.rb
@@ -21,7 +21,7 @@ end
 
 cass_client = Cassandra.new "Hastur", opts[:cassandra].flatten
 
-ROUTES = Hastur::Cassandra::SCHEMA.keys
+TYPES = Hastur::Cassandra::SCHEMA.keys
 
 def check_present(param_name, human_name = nil)
   unless params[param_name]
@@ -35,19 +35,19 @@ def check_absent(param_name, human_name = nil)
   end
 end
 
-before "/data/:route/*" do
-  if params[:route]
-    params[:route] = params[:route].downcase
-    unless ROUTES.include?(params[:route])
+before "/data/:TYPE/*" do
+  if params[:type]
+    params[:type] = params[:type].downcase
+    unless TYPES.include?(params[:type])
       halt 404, <<EOJSON
-{ "msg": "Route must be one of: #{ROUTES.join ', '}" }
+{ "msg": "Type must be one of: #{TYPES.join ', '}" }
 EOJSON
     end
   end
 end
 
 #
-# This route returns JSON message objects for the specified route at the
+# This route returns JSON message objects for the specified type at the
 # given timestamps.
 #
 # Data is returned in a JSON object of the form:
@@ -61,20 +61,20 @@ end
 #
 # TODO(noah): Fix these return types to avoid double-decode
 #
-get "/data/:route/json" do
+get "/data/:type/json" do
   [ :start, :end, :uuid ].each { |p| check_present p }
 
   start_ts = Hastur.timestamp(params[:start].to_i)
   end_ts = Hastur.timestamp(params[:end].to_i)
 
   # Get with no subtype gives JSON
-  values = Hastur::Cassandra.get(cass_client, params[:uuid], params[:route], start_ts, end_ts)
+  values = Hastur::Cassandra.get(cass_client, params[:uuid], params[:type], start_ts, end_ts)
 
   [ 200, MultiJson.encode(values) ]
 end
 
 #
-# This route returns values for the given route at the given
+# This route returns values for the given type at the given
 # timestamps.
 #
 # Data is returned in a JSON object of the form:
@@ -83,19 +83,19 @@ end
 #     "name2" => { "ts5" => value5, "ts6" => value6}
 #   }
 #
-get "/data/:route/values" do
+get "/data/:type/values" do
   [ :start, :end, :uuid ].each { |p| check_present p }
 
-  unless [ "stat", "heartbeat" ].include?(params[:route])
+  unless [ "stat", "heartbeat" ].include?(params[:type])
     halt 404, <<EOJSON
-{ "msg": "Can only get values for routes: stat, heartbeat" }
+{ "msg": "Can only get values for types: stat, heartbeat" }
 EOJSON
   end
 
   subtype_list = []
-  if params[:route] != "stat" && !params[:subtype]
+  if params[:type] != "stat" && !params[:subtype]
     subtype_list = [ "" ]  # Subtype is harmless when unsupported
-  elsif params[:subtype] && params[:route] == "stat"
+  elsif params[:subtype] && params[:type] == "stat"
     unless [ "gauge", "counter", "mark" ].include?(params[:subtype])
       halt 404, <<EOJSON
 { "msg": "Subtype must be one of: gauge, counter, mark" }
@@ -115,7 +115,7 @@ EOJSON
 
   values = {}
   subtype_list.each do |subtype|
-    value = Hastur::Cassandra.get(cass_client, params[:uuid], params[:route],
+    value = Hastur::Cassandra.get(cass_client, params[:uuid], params[:type],
                                   start_ts, end_ts, :subtype => subtype)
     values.merge!(value)
   end
@@ -123,7 +123,7 @@ EOJSON
   [ 200, MultiJson.encode(values) ]
 end
 
-get "/data/:route/rollups" do
+get "/data/:type/rollups" do
   [ :start, :end, :uuid, :granularity ].each { |p| check_present p }
 
 
@@ -141,7 +141,7 @@ get "/uuids" do
   [ 200, MultiJson.encode(data.values.map(&:keys).flatten) ]
 end
 
-get "/names/:route" do
+get "/names/:type" do
   [ :start, :end ].each { |p| check_present p }
 
   [ 200, "" ]
