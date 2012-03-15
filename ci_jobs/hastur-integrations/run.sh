5ed05202da5bc272c8c67bf1f14dd6f4de4d22eb
Al Tobey <al@ooyala.com>
2012-03-14 17:17:42 -0700

    [debugging] Print ulimits when the test starts.

--- ci_jobs/hastur-integrations/run.sh
+++ ci_jobs/hastur-integrations/run.sh
@@ -4,6 +4,10 @@ set +e
 
 unset CASSANDRA_HOME
 
+echo "---------------------------------------"
+ulimit -a
+echo "---------------------------------------"
+
 : ${REPO_ROOT:="$WORKSPACE"}
 source $HOME/.rvm/scripts/rvm
 
@@ -13,5 +17,10 @@ gem install --no-rdoc --no-ri bundler
 bundle install
 gem build hastur-server.gemspec
 gem install hastur-server-*.gem
+
+echo "
+===============================================================================
+===============================================================================
+"
 
 COVERAGE=true bundle exec rake --trace test:integrations
