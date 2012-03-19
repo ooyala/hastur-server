6f0a62974453f4adbdd60c75c3d91147c800f1c6
Jay Bhat <jbhat@ooyala.com>
2012-03-19 15:37:34 -0700

    put in boilerplate for adding sink stack back to this test

--- test/integration/bring_up_test.rb
+++ test/integration/bring_up_test.rb
@@ -28,6 +28,14 @@ class BringUpTest < Test::Unit::TestCase
       :rawdata      => Nodule::ZeroMQ.new(:connect => ZMQ::PULL, :uri => :gen, :reader => :drain),
       :control      => Nodule::ZeroMQ.new(:connect => ZMQ::REP,  :uri => :gen),
       :direct       => Nodule::ZeroMQ.new(:connect => ZMQ::PUSH, :uri => :gen),
+      :cassandra    => Nodule::Cassandra.new(:keyspace => "Hastur"),
+      :query_server => Nodule::Process.new(HASTUR_QUERY_SERVER_BIN,
+        '--cassandra', :cassandra,
+        '--', '-p', '4177',
+        :stdout => :greenio, :stderr => proc do |line|
+          sinatra_ready = true if !sinatra_ready && line =~ /== Sinatra.* has taken the stage/
+        end,
+      ),
       :client1svc   => Nodule::Process.new(
         HASTUR_CLIENT_BIN, '--uuid', C1UUID, '--heartbeat', 1, '--router', :router,
         :stdout => :greenio, :stderr => :redio, :verbose => :yellowio,
@@ -68,34 +76,61 @@ class BringUpTest < Test::Unit::TestCase
         '--hwm',          10,   # Set HWM so this doesn't 'clog'
         :stdout => :greenio, :stderr => :redio, :verbose => :cyanio
       ),
+      :cass_sink1 => Nodule::Process.new(
+        HASTUR_CASS_SINK_BIN,
+        '--sinks', :stat, :event, :heartbeat, :registration, :log, :error,
+        '--cassandra', :cassandra,
+        :verbose => :cyanio, :stderr => :redio, :stdout => :yellowio
+      ),
+      :cass_sink2 => Nodule::Process.new(
+        HASTUR_CASS_SINK_BIN,
+        '--sinks', :stat, :event, :heartbeat, :registration, :log, :error,
+        '--cassandra', :cassandra,
+        :verbose => :cyanio, :stderr => :redio, :stdout => :yellowio
+      ),
     )
 
-    @topology.start_all_but :client2svc, :router2svc
+    # @topology.start :cassandra
+    # create_all_column_families(@topology[:cassandra]) # helper
+    @topology.start_all_but :client2svc, :router2svc, :cass_sink1, :cass_sink2, :query_server, :cassandra
+    # sleep 0.01 until sinatra_ready
   end
 
   def teardown
-    @topology.stop_all
+    # @topology.stop :cass_sink1, :cass_sink2
+    @topology.stop_all_but :cass_sink1, :cass_sink2, :query_server, :cassandra
   end
 
   def test_bring_up
-    # TODO: some of the tests below may have to change, since the clients will continue to send heartbeats
-    # with this method of sync.
+    # Initial testing
     @topology[:heartbeat].require_read_count 2, 5
 
     messages = @topology[:heartbeat].output
-    # First, check messages
-    payloads  = messages.map { |m| MultiJson.decode(m[-1]) }
-    envelopes = messages.map { |m| m[-2].unpack("H*") }
+    assert_not_empty messages
 
-    STDERR.puts "Heartbeat message(s): #{messages.inspect}"
-
+    # Start up and test second client
     @topology.start :client2svc
     @topology[:registration].require_read_count 2, 5
 
+    # Start up and test second router
     @topology.start :router2svc
     Hastur.counter("My.counter")
     Hastur.udp_port = 8124
     Hastur.counter("My.counter")
     @topology[:stat].require_read_count 2,5
+
+    # # Test query server
+    # # Query from 10 minutes ago to 10 minutes from now, just to grab everything
+    # start_ts = Hastur.timestamp(Time.now.to_i - 600)
+    # end_ts = Hastur.timestamp(Time.now.to_i + 600)
+    #
+    # c1_messages = MultiJson.decode `curl http://localhost:4177/data/heartbeat/json?uuid=#{C1UUID}\\\&start=#{start_ts}\\\&end=#{end_ts}`
+    # c2_messages = MultiJson.decode `curl http://localhost:4177/data/heartbeat/values?uuid=#{C2UUID}\\\&start=#{start_ts}\\\&end=#{end_ts}`
+    #
+    # assert_not_empty c1_messages
+    # assert_not_empty c2_messages
+
+    # Start up and test second sink
+    # @topology.start :cass_sink2
   end
 end
