47f8ac7180882dd85e744cb3fa77d781c46eafc1
Noah Gibbs <noah@ooyala.com>
2012-03-20 07:28:30 -0700

    Uninstall bundler 1.1.1, install version 1.1.0

--- ci_jobs/hastur-integrations/run.sh
+++ ci_jobs/hastur-integrations/run.sh
@@ -15,7 +15,8 @@ source $HOME/.rvm/scripts/rvm
 
 cd $REPO_ROOT/hastur-server
 rvm --create use 1.9.3@hastur-server
-gem install --no-rdoc --no-ri bundler
+gem uninstall bundler -v 1.1.1
+gem install --no-rdoc --no-ri bundler -v 1.1.0
 bundle install
 gem build hastur-server.gemspec
 gem install hastur-server-*.gem
--- ci_jobs/hastur-units/run.sh
+++ ci_jobs/hastur-units/run.sh
@@ -5,6 +5,7 @@ source $HOME/.rvm/scripts/rvm
 
 cd $REPO_ROOT/hastur-server
 rvm --create use 1.9.3@hastur-server
-gem install --no-rdoc --no-ri bundler
+gem uninstall bundler -v 1.1.1
+gem install --no-rdoc --no-ri bundler -v 1.1.0
 bundle install
 COVERAGE=true bundle exec rake --trace test:units:full
