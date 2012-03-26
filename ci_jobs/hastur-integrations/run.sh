e0170776cab114e7b2c93efe6826d5f565a3d2c8
Noah Gibbs <noah@ooyala.com>
2012-03-26 14:53:13 -0700

    Try using Bundler 1.1.3 -- did it fix the 1.1.1 bug?

--- ci_jobs/hastur-integrations/run.sh
+++ ci_jobs/hastur-integrations/run.sh
@@ -18,7 +18,7 @@ source $HOME/.rvm/scripts/rvm
 cd $REPO_ROOT/hastur-server
 rvm --create use 1.9.3@hastur-server
 gem uninstall bundler -v 1.1.1
-gem install --no-rdoc --no-ri bundler -v 1.1.0
+gem install --no-rdoc --no-ri bundler
 bundle install
 gem build hastur-server.gemspec
 gem install hastur-server-*.gem
