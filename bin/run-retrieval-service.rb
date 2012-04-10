a34ce099b86879b769d5ad1abc454a6838a35486
Viet Nguyen <vhnguy2@gmail.com>
2012-04-09 18:51:25 -0700

    Add run script for the retrieval service.

---
+++ bin/run-retrieval-service.rb
@@ -0,0 +1,11 @@
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), "..", "web")
+
+require "rack"
+require "hastur-retrieval-service"
+
+CASSANDRA_URIS = ["127.0.0.1:9160"]
+
+Rack::Handler::WEBrick.run(
+  Hastur::RetrievalApp.new( CASSANDRA_URIS ),
+  :Port => 9000
+)
