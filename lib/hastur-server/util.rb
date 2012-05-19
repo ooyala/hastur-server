ca464b9035256703c9c447fe611570b265850d96
Al Tobey <al@ooyala.com>
2012-05-18 18:33:02 -0700

    [util] convert to use TimeUtil, deprecate .timestamp

--- lib/hastur-server/util.rb
+++ lib/hastur-server/util.rb
@@ -1,20 +1,11 @@
 require "multi_json"
 require "termite"
+require "hastur-server/time_util"
 
 # A collection of useful functions, including for the ffi-rzmq gem.
 module Hastur
   module Util
     extend self  # Allows calling as Util.blah
-
-    SECS_2100       = 4102444800
-    MILLI_SECS_2100 = 4102444800000
-    MICRO_SECS_2100 = 4102444800000000
-    NANO_SECS_2100  = 4102444800000000000
-    SECS_1971       = 31536000
-    MILLI_SECS_1971 = 31536000000
-    MICRO_SECS_1971 = 31536000000000
-    NANO_SECS_1971  = 31536000000000000
-
     #
     # Get a Termite logger (singleton) handle.
     #
@@ -25,31 +16,11 @@ module Hastur
     end
 
     #
-    # Best effort to make all timestamps be Hastur timestamps, 64 bit
-    # numbers that represent the total number of microseconds since Jan
-    # 1, 1970 at midnight UTC.  Default to giving Time.now as a Hastur
-    # timestamp.
+    # @see Hastur::TimeUtil.usec_epoch
+    # @deprecated Use Hastur::TimeUtil.usec_epoch instead.
     #
-    # @param [Time, Fixnum] ts default Time.now
-    # @return [Fixnum] epoch microseconds
-    #
-    def timestamp(ts=Time.now)
-      case ts
-        when nil, ""
-          (Time.now.to_f * 1_000_000).to_i
-        when Time;
-          (ts.to_f * 1_000_000).to_i
-        when SECS_1971..SECS_2100
-          ts * 1_000_000
-        when MILLI_SECS_1971..MILLI_SECS_2100
-          ts * 1_000
-        when MICRO_SECS_1971..MICRO_SECS_2100
-          ts
-        when NANO_SECS_1971..NANO_SECS_2100
-          ts / 1_000
-        else
-          raise "Unable to convert timestamp: #{ts} (class: #{ts.class})"
-      end
+    def timestamp(*args)
+      Hastur::TimeUtil.usec_epoch *args
     end
 
     # application boot time in epoch microseconds, intentionally not system boot time
