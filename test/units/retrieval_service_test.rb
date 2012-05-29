6599e9dfec9a92dd25a0bd4d112ee6b51f6d66bf
Noah Gibbs <noah@ooyala.com>
2012-05-29 11:05:00 -0700

    Add types test

--- test/units/retrieval_service_test.rb
+++ test/units/retrieval_service_test.rb
@@ -11,11 +11,25 @@ class RetrievalServiceTest < MiniTest::Unit::TestCase
     Hastur::Service::Retrieval.new []
   end
 
-  def test_blah
-    get "/api"
-    # follow_redirect!
+  def get_response_hash(uri, options = {})
+    get uri
+    #follow_redirect!
 
-    assert last_response.ok?, "Request must succeed"
-    assert MultiJson.load(last_response.body).keys.size == 5, "Must have five keys"
+    assert last_response.ok?, "Request to #{uri} must succeed"
+    MultiJson.load(last_response.body)
+  end
+
+  def test_top_level
+    hash = get_response_hash "/api"
+
+    assert hash.keys.include?("node"), "Must contain Node URL"
+  end
+
+  def test_types
+    hash = get_response_hash "/api/type"
+
+    assert hash.keys.include?("all"), "Types hash must include 'all'"
+    assert hash.keys.include?("stat"), "Types hash must include 'stat'"
+    assert hash.keys.include?("event"), "Types hash must include 'event'"
   end
 end
