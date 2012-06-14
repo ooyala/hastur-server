86d54af17c18c5798119f5da90568817c7367dc7
Al Tobey <al@ooyala.com>
2012-06-14 15:01:18 -0700

    add proper arg parsing to zones2cassandra.pl

--- tools/zones2cassandra.pl
+++ tools/zones2cassandra.pl
@@ -13,9 +13,16 @@ using a reverse mapping built from our zone files.
 This is written in perl instead of ruby because most of the DNS side already existed
 and the perl parser is more reliable.
 
+ zones2cassandra.pl -h hastur-cassandra1 -p 9202 -z $CODE_ROOT/dns/zones/pri/foo.zone
+
 =head1 SYNOPSIS
 
-perl zones2cassandra.pl $CODE_ROOT/dns/zones/pri/foo.zone
+ zones2cassandra.pl -z <zonefile> [-h localhost] [-p 9160] [-k Hastur]
+ zones2cassandra.pl --zonefile <zonefile> [--host localhost] [--port 9160] [--keyspace Hastur]
+   --zonefile <filename> BIND DNS zonefile to parse and write to Cassandra
+   --host <hostname> Cassandra hostname to connect to
+   --port <port> Cassandra RPC port
+   --keyspace <keyspace> Cassandra keyspace to write to
 
 =cut
 
@@ -28,25 +35,50 @@ use IO::String;
 use File::Slurp;
 use Cassandra::Lite;
 use DateTime;
+use Getopt::Long;
+use Pod::Usage;
 
-our %names;
+pod2usage(-exit => 1) if (@ARGV == 0);
 
-my $c = Cassandra::Lite->new(
-    keyspace => "Hastur",
-    server_port => 9202,
+our($opt_keyspace, $opt_host, $opt_port, @opt_zonefile, $opt_help, $opt_debug);
+GetOptions(
+  "k:s" => \$opt_keyspace, "keyspace:s" => \$opt_keyspace,
+  "h:s" => \$opt_host,     "host:s"     => \$opt_host,
+  "p:s" => \$opt_port,     "port:s"     => \$opt_port,
+  "z:s" => \@opt_zonefile, "zonefile:s" => \@opt_zonefile,
+  "d"   => \$opt_debug,    "debug"      => \$opt_debug,
+                           "help"       => \$opt_help
+);
+
+our $keyspace = $opt_keyspace || "Hastur";
+our $host     = $opt_host     || "localhost";
+our $port     = $opt_port     || 9160;
+
+pod2usage(-exit => 0) if ($opt_help);
+pod2usage(-message => "you must specify at least one zonefile", -exit => 1)
+  unless @opt_zonefile > 0;
+
+our $c = Cassandra::Lite->new(
+    keyspace    => $keyspace,
+    server_port => $port,
+    server_name => $host,
     consistency_level_write => 'TWO'
 );
 
-my $today_usec = DateTime->today->epoch * 1_000_000;
+our $today_usec = DateTime->today->epoch * 1_000_000;
 
-foreach my $zonefile ( @ARGV ) {
+print "Generating row $keyspace/LookupByKey/cnames-$today_usec from @opt_zonefile ...\n";
+
+our %names;
+foreach my $zonefile ( @opt_zonefile ) {
     my $path = dirname($zonefile);
 
     # read the data in and remove inconvenient lines like includes
+    # which break the parser for now
     my $zone_txt = read_file($zonefile);
     $zone_txt =~ s/\$include[^\n]+\n//gi;
-    my $fh = IO::String->new($zone_txt);
 
+    my $fh = IO::String->new($zone_txt);
     my $rr = Net::DNS::ZoneFile::Fast::parse( fh => $fh );
 
     foreach my $r ( @$rr ) {
@@ -60,6 +92,11 @@ foreach my $zonefile ( @ARGV ) {
     }
 }
 
-#$c->delete("LookupByKey", "cnames-$today_usec");
+print "Writing to Cassandra $keyspace/LookupByKey/cnames-$today_usec ...\n";
+
+if ($opt_debug) {
+  print Dumper(\%names);
+}
+
 $c->put("LookupByKey", "cnames-$today_usec", \%names);
 
