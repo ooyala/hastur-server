2e73b08e795640e45d2ea0c84e84908edad5cbb4
Al Tobey <al@ooyala.com>
2012-07-13 15:45:40 -0700

    Add format() function to replace all formats, add array format.

--- lib/hastur-server/aggregation/formats.rb
+++ lib/hastur-server/aggregation/formats.rb
@@ -5,7 +5,19 @@ module Hastur
   module Aggregation
     extend self
 
-    @functions.merge! "format_cubism" => :format_cubism
+    @functions.merge!({
+      "format" => :format,
+      # deprecated, switch to format(cubism,...)
+      "format_cubism" => :format_cubism
+    })
+
+    def format(series, control, which_format="none")
+      case which_format
+      when "cubism" ; format_cubism series, control
+      when "array"  ; format_array series, control
+      else ; series
+      end
+    end
 
     def format_cubism(series, control, *args)
       out = []
@@ -18,6 +30,20 @@ module Hastur
         end
       end
       return out, control
+    end
+
+    def format_array(series, control, *args)
+      new_series = {}
+      series.each do |uuid, name_series|
+        new_series[uuid] = {}
+        name_series.each do |name, subseries|
+          new_series[uuid][name] = []
+          subseries.each do |ts, val|
+            new_series[uuid][name] << { :timestamp => ts, :value => val }
+          end
+        end
+      end
+      return new_series, control
     end
   end
 end
