15b39e71a8e3134104d3bee9999894e7a35ad75b
Al Tobey <al@ooyala.com>
2012-08-07 12:10:26 -0700

    Run the agent as a user in production.
    Plugins kinda needed root, but what's left after they're gone doesn't.
    
    The after-install script for the Debian package installs the role-hastur user
    automatically and bluepill will now run as that user.
    
    We pretty much always test the agent as a non-root user, so this is a fairly
    safe change. The package is the main thing to test, which I will do manually
    during the next package build cycle.

--- build/scripts/after-install.sh
+++ build/scripts/after-install.sh
@@ -4,6 +4,10 @@
 # so it's buried in /opt/hastur. Find the first occurrence and symlink that.
 initscript=$(find /opt/hastur -type f -name hastur-bluepill.init | head -n 1)
 
+# create the user if necessary
+(grep -q '^role-hastur:' /etc/group)  || groupadd -g 60442 role-hastur
+(grep -q '^role-hastur:' /etc/passwd) || useradd -r -o -u 60442 -c "Hastur Metrics Agent" -g role-hastur -s /bin/false role-hastur
+
 if [ -n "$initscript" ] ; then
   ln -nfs $initscript /etc/init.d/hastur-agent.init
   update-rc.d hastur-agent.init defaults
