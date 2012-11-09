fa53326fb40dd42b227cb46374d3aa600f1e0e7b
Al Tobey <al@ooyala.com>
2012-11-09 14:53:49 -0800

    [message] use ZMQ::Util.errno, not ZMQ.errno

--- lib/hastur-server/message.rb
+++ lib/hastur-server/message.rb
@@ -144,7 +144,7 @@ module Hastur
       if rc == -1
         return rc if zmq_flags != 0 && ZMQ.errno == ZMQ::EAGAIN  # NonBlocking, got EAGAIN
         return rc if tries >= MAX_TRIES                          # EINTR (syscall interrupted) too many times
-        return recv(socket, zmq_flags, only_test_success, tries) if ZMQ.errno == ZMQ::EINTR
+        return recv(socket, zmq_flags, only_test_success, tries) if ZMQ::Util.errno == ZMQ::EINTR
 
         raise ZMQError.new "ZMQ recvmsgs failed: '#{ZMQ::Util.error_string}'"
       end
