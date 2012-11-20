dbcac477f701da300e130ec14a129f0c29145174
Noah Gibbs <noah@ooyala.com>
2012-11-20 09:51:05 -0800

    Warbler: enable C extensions

--- config/warble.rb
+++ config/warble.rb
@@ -201,6 +201,8 @@ Warbler::Config.new do |config|
   # Set JRuby to run in 1.9 mode.
   config.webxml.jruby.compat.version = "1.9"
 
+  config.webxml.jruby.cext.enabled = true
+
   # When using the :rack booter, "Rackup" script to use.
   # - For 'rackup.path', the value points to the location of the rackup
   # script in the web archive file. You need to make sure this file
