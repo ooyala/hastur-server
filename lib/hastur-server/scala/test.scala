2ed1917d68e0d8d70ad6b8c22b2895e782ab4ac9
Noah Gibbs <noah@ooyala.com>
2012-11-30 06:27:50 -0800

    Scala: remove test mainClass

--- lib/hastur-server/scala/test.scala
+++ lib/hastur-server/scala/test.scala
@@ -2,8 +2,4 @@ class ScalaMain {
   def scalaString = "String from Scala"
 
   def scalaComputation(arg: java.lang.Integer) = arg + 2
-
-  def main(args: Array[String]) {
-    println("This is more of a library, really.")
-  }
 }
