61a7a9de1f927353b5615729e09ae5f19748175c
Noah Gibbs <noah@ooyala.com>
2013-01-01 15:00:24 -0800

    Tests: add retrieval test for well-specified non-label query

--- test/units/retrieval_test.rb
+++ test/units/retrieval_test.rb
@@ -1,8 +1,20 @@
 require_relative "../test_helper"
 require "rack/test"
 
+require "multi_json"
+MultiJson.use :json_gem
+
 require "hastur-server/api/v2"
 require "hastur-server/api/cass_java_client"
+
+# Timestamp in seconds
+NOWISH_TIMESTAMP = 1330000400000000
+
+# Timestamps in microseconds, rounded down to various sizes.
+ROW_5MIN_TS = 1329858600000000
+ROW_HOUR_TS = 1329858000000000
+ONE_DAY = 24 * 60 * 60 * 1_000_000
+ROW_DAY_TS = Hastur::Cassandra.send(:time_segment_for_timestamp, ROW_5MIN_TS, ONE_DAY).to_s
 
 class RetrievalServerTest < Scope::TestCase
   include Rack::Test::Methods
@@ -15,6 +27,8 @@ class RetrievalServerTest < Scope::TestCase
     @cass_client = mock("Cass client")
     ::Hastur::API::CassandraJavaClient.stubs(:new).with([]).returns(@cass_client)
     @cass_client.stubs(:status_check)
+
+    Hastur.stubs(:timestamp).returns(NOWISH_TIMESTAMP)
   end
 
   should "raise no error on /statusz" do
@@ -24,9 +38,23 @@ class RetrievalServerTest < Scope::TestCase
 
   context "non-label query" do
     should "do simple lookup for fully-specified query" do
-      #refute_nil sum
-      #assert_kind_of Hash, sum
-      #assert_equal UUID1_BAZ_SUM, sum[UUID1]["foo.bar.baz"].values.first
+      out_hash = {
+        A1UUID => {
+          "bobs.gauge" => {
+            NOWISH_TIMESTAMP => 37
+          }
+        }
+      }
+
+      Hastur::Cassandra.expects(:get).with(@cass_client, [A1UUID], ["gauge"],
+                                           NOWISH_TIMESTAMP - 1, NOWISH_TIMESTAMP, {
+        :name => "bobs.gauge",
+        :value_only => true,
+        :request_ts => NOWISH_TIMESTAMP}).returns(out_hash)
+
+      result = get "/v2/query?type=gauge&ago=1&uuid=#{A1UUID}&name=bobs.gauge&kind=value"
+      hash = MultiJson.load(result.body)
+      assert_equal 37, hash[A1UUID]["bobs.gauge"][NOWISH_TIMESTAMP.to_s]
     end
   end
 end
