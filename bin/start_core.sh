8477a91b311aa0dea7459ab426026eb0fb11a005
Noah Gibbs <noah@ooyala.com>
2013-01-15 12:27:34 -0800

    Server: add commented-out Cass configuration for varying Cassandra clusters

--- bin/start_core.sh
+++ bin/start_core.sh
@@ -3,6 +3,12 @@
 export JAVA_OPTS="-XX:+UseParNewGC -XX:+UseAdaptiveSizePolicy -XX:MaxGCPauseMillis=100 -XX:GCTimeRatio=19"
 export RESOURCE_OPTS="-Xmx1g"
 
+# Configure Cassandra this way
+# export HASTUR_CASS_PORT=9161
+# export HASTUR_CASS_CLUSTER=aCluster
+# export HASTUR_CASS_USER=hastur
+# export HASTUR_CASS_PASSWD=hasturPassword
+
 pkill -9 -f core.jar
 
 export JAVA_CMD="java -jar core.jar $RESOURCE_OPTS $JAVA_OPTS --router tcp://0.0.0.0:8126"
--- bin/start_retrieval.sh
+++ bin/start_retrieval.sh
@@ -3,6 +3,12 @@
 export JAVA_OPTS="-XX:+UseParNewGC -XX:+UseAdaptiveSizePolicy -XX:MaxGCPauseMillis=100 -XX:GCTimeRatio=19"
 export RESOURCE_OPTS="-Xmx5g"
 
+# Configure Cassandra this way
+# export HASTUR_CASS_PORT=9161
+# export HASTUR_CASS_CLUSTER=aCluster
+# export HASTUR_CASS_USER=hastur
+# export HASTUR_CASS_PASSWD=hasturPassword
+
 pkill -9 -f retrieval_v2.war
 
 export JAVA_CMD="java -jar retrieval_v2.war $RESOURCE_OPTS $JAVA_OPTS"
