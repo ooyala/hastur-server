4a1fcc17d86d4c17f6c3b72e454a91ba61a4978e
Noah Gibbs <noah@ooyala.com>
2013-01-15 12:21:33 -0800

    Debug new server packaging scripts and make them executable

--- build/scripts/package_server.sh
+++ build/scripts/package_server.sh
@@ -3,25 +3,31 @@
 # This builds a deployable file to copy to the Hastur core/retrieval servers.
 # Run it from root of hastur-server project by calling bin/package_server.sh.
 
-cp bin/install_server.sh build/server/
+rm -rf build/server
+mkdir build/server || echo "Directory already exists!"
 
-# TODO: move all these scripts under build/
+cp build/scripts/install_server.sh build/server/
 
 # Build jars, copy into build/server/
 rm -f jars/*.?ar
 mkdir -p build/server
 rake core_jar
-cp core.jar build/server/
+mv build/jars/core.jar build/server/
 rake retrieval_war
-cp retrieval_v2.war build/server/
+mv build/jars/retrieval_v2.war build/server/
 
 # Hastur Core scripts
-cp bin/hastur-core.conf build/server/
+cp build/scripts/hastur-core.conf build/server/
 cp bin/start_core.sh build/server/
 
 # Hastur Retrieval v2 scripts
-cp bin/hastur-retrieval.conf build/server/
+cp build/scripts/hastur-retrieval.conf build/server/
 cp bin/start_retrieval.sh build/server/
 
 # Manually-runnable removal scripts for old versions
-cp bin/remove*.sh build/server/
+cp build/scripts/remove*.sh build/server/
+
+tar jcvf server_package.tar.bz build/server
+
+echo "TODO: don't echo, do"
+echo "rm -rf build/server"
