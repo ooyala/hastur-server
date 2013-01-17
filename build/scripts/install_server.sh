79cd6d024e782a365a0a006c61e079594bb8d727
Noah Gibbs <noah@ooyala.com>
2013-01-17 11:11:14 -0800

    More debugging server install

--- build/scripts/install_server.sh
+++ build/scripts/install_server.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+# Run as root, or with "sudo ./install_server.sh"
+
+# Install or upgrade Hastur agent
+curl http://apt.us-east-1.ooyala.com/hastur.sh |sudo bash -s
+
+if ! [ -f /etc/uuid ]
+  then uuidgen > /etc/uuid
+fi
+
 mkdir -p /opt/hastur-server
 cp ./*.?ar /opt/hastur-server/
 cp ./start* /opt/hastur-server/
+chmod +x /opt/hastur-server/start*
 cp *.conf /etc/init/
 
-# Upstart
-start /etc/init/hastur-core.conf
-start /etc/init/hastur-retrieval.conf
+# To get the job to re-read the config file, stop and start (not restart)
+stop hastur-core
+stop hastur-retrieval
+start hastur-core
+start hastur-retrieval
