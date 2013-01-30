a9ded138bf2a4b9f34254f09546e289aeba45825
Noah Gibbs <noah@ooyala.com>
2013-01-30 10:30:39 -0800

    Cassandra: fix port number calculation

--- lib/hastur-server/api/cass_java_client.rb
+++ lib/hastur-server/api/cass_java_client.rb
@@ -5,7 +5,9 @@ module Hastur
     # This uses JRuby-Astyanax to implement something that looks like the Cassandra gem client
     class CassandraJavaClient
       def initialize(uris)
-        @ast_client = ::Astyanax::Client.new uris, ENV["HASTUR_CASS_PORT"].to_i || 9160,
+        STDERR.puts "CONNECTING TO URIS: #{uris.inspect}"
+        default_port = ENV["HASTUR_CASS_PORT"] || 9160
+        @ast_client = ::Astyanax::Client.new uris, default_port.to_i,
           ENV["HASTUR_CASS_CLUSTER"] || "aCluster", ENV["HASTUR_CASS_USER"], ENV["HASTUR_CASS_PASSWD"]
         @keyspace = @ast_client.connect("hastur", :discovery => true, :connect_timeout_ms => 10_000)
         @java_keyspace = @keyspace.java_keyspace
