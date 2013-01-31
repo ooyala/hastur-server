0b1be5b4b7f0bb9fbf98c323a7e57cde4ecdb716
Noah Gibbs <noah@ooyala.com>
2013-01-31 11:09:01 -0800

    First draft conversion to rbenv -- expect dependency problems'

--- ci_jobs/hastur-units/run.sh
+++ ci_jobs/hastur-units/run.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-: ${REPO_ROOT:="$WORKSPACE"}
-source $HOME/.rvm/scripts/rvm
+#!/bin/bash
+set -ex
 
-export JRUBY_OPTS="-Xcext.enabled=true"
+eval "$(rbenv init -)"
 
-cd $REPO_ROOT/hastur-server
-rvm list | grep jruby-1.7.0 || rvm install jruby-1.7.0
-rvm --create use jruby-1.7.0@hastur-server
-gem uninstall bundler -v 1.1.1
-gem install --no-rdoc --no-ri bundler -v 1.1.0
-bundle update   # Update to latest versions since this is a gem
-#bundle install
+# The current working directory is exactly WORKSPACE.
+# This is where each repo the test requires is checked out to.
+: ${OOYALA_REPO_ROOT:="$WORKSPACE"}
+
+# Setup a 1.9.2-p290 test environment
+rbenv shell jruby-1.7.0
+
+gem install --no-rdoc --no-ri bundler
+
+# Move to the project repo
+cd $WORKSPACE/hastur
+
+# install the necessary gems and execute tests
+bundle update
+
+# just in case we installed some executables...
+rbenv rehash
 
 rake native_jar
 
-COVERAGE=true bundle exec rake test:units:full --trace
+COVERAGE=true bundle exec rake --trace test:units:full
